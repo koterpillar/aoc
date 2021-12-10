be559b231177aadecdce4fdbcdd62cc845ad10d2
Alexey Kotlyarov <a@koterpillar.com>
2021-12-10 20:59:05 +1100

    Day 08 part 2

--- day08.hs
+++ day08.hs
@@ -1,6 +1,11 @@
 import           Control.Monad    (join)
 
+import           Data.Foldable
+
 import           Data.Functor
+
+import           Data.Map         (Map)
+import qualified Data.Map         as Map
 
 import           Data.Maybe
 
@@ -23,7 +28,10 @@ data Wire
   | E
   | F
   | G
-  deriving (Ord, Eq, Show)
+  deriving (Ord, Eq, Show, Enum)
+
+wires :: [Wire]
+wires = [A .. G]
 
 type Display = Set Wire
 
@@ -32,12 +40,61 @@ data Reading =
   deriving (Ord, Eq, Show)
 
 guessNumbers :: Set Display -> Display -> Maybe Int
-guessNumbers _ display
-  | Set.size display == 2 = Just 1
-  | Set.size display == 4 = Just 4
-  | Set.size display == 3 = Just 7
-  | Set.size display == 7 = Just 8
-  | otherwise = Nothing
+guessNumbers allnumbers = flip Map.lookup result
+  where
+    findBySize n cs =
+      case find (\x -> Set.size x == n) cs of
+        Nothing -> error $ "Cannot find set with " <> show n <> " elements"
+        Just c  -> c
+    n1 = findBySize 2 allnumbers
+    n4 = findBySize 4 allnumbers
+    n7 = findBySize 3 allnumbers
+    n8 = findBySize 7 allnumbers
+    single :: (Foldable f, Show a) => String -> f a -> a
+    single msg s =
+      case toList s of
+        [x]   -> x
+        other -> error $ "Cannot choose " <> msg <> " from " <> show other
+    a = single "a" $ Set.difference n7 n1
+    happensIn = happensIn' allnumbers
+    happensIn' displays n =
+      single ("occurs " <> show n <> " times") $ happenIn displays n
+    happenIn displays n =
+      filter (\w -> countIf (Set.member w) (Set.toList displays) == n) wires
+    b = happensIn 6
+    e = happensIn 4
+    f = happensIn 9
+    c = single "c" $ Set.difference n1 (Set.singleton f)
+    d = single "d" $ Set.difference n4 $ Set.union n1 $ Set.singleton b
+    g =
+      single "g" $
+      Set.difference (Set.fromList wires) (Set.fromList [a, b, c, d, e, f])
+    n0 = Set.fromList [a, b, c, e, f, g]
+    n2 = Set.fromList [a, c, d, e, g]
+    n3 = Set.fromList [a, c, d, f, g]
+    n5 = Set.fromList [a, b, d, f, g]
+    n6 = Set.fromList [a, b, d, e, f, g]
+    n9 = Set.fromList [a, b, c, d, f, g]
+    result =
+      Map.fromList
+        [ (n0, 0)
+        , (n1, 1)
+        , (n2, 2)
+        , (n3, 3)
+        , (n4, 4)
+        , (n5, 5)
+        , (n6, 6)
+        , (n7, 7)
+        , (n8, 8)
+        , (n9, 9)
+        ]
+
+readDisplay :: Reading -> Int
+readDisplay (Reading allnumbers digits) =
+  foldl (\n d -> n * 10 + d) 0 digitsValues
+  where
+    guess = guessNumbers allnumbers
+    digitsValues = map (fromJust . guess) digits
 
 readingP :: Parser Reading
 readingP =
@@ -75,6 +132,10 @@ part1 = sum . map countSimple
     isSimple 8 = True
     isSimple _ = False
 
+part2 :: [Reading] -> Int
+part2 = sum . map readDisplay
+
 main :: IO ()
 main = do
   processEI 8 (justParse readingsP) part1 26
+  processEI 8 (justParse readingsP) part2 61229
