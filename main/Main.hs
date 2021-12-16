c6848873c178eb4040e209dbe75fa7df6934b23f
Alexey Kotlyarov <a@koterpillar.com>
2021-12-16 22:30:43 +1100

    Automatically enumerate all tasks

--- main/Main.hs
+++ main/Main.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -F -pgmF./enumerate_days #-}
+
 import qualified Data.Map           as Map
 import qualified Data.Text          as Text
 
@@ -6,42 +8,7 @@ import           System.Environment (getArgs)
 import           AOC
 import           Utils
 
-import qualified Y2021.Day01
-import qualified Y2021.Day02
-import qualified Y2021.Day03
-import qualified Y2021.Day04
-import qualified Y2021.Day05
-import qualified Y2021.Day06
-import qualified Y2021.Day07
-import qualified Y2021.Day08
-import qualified Y2021.Day09
-import qualified Y2021.Day10
-import qualified Y2021.Day11
-import qualified Y2021.Day12
-import qualified Y2021.Day13
-import qualified Y2021.Day14
-import qualified Y2021.Day15
-import qualified Y2021.Day16
-
-allTasks :: [Tasks]
-allTasks =
-  [ Y2021.Day01.tasks
-  , Y2021.Day02.tasks
-  , Y2021.Day03.tasks
-  , Y2021.Day04.tasks
-  , Y2021.Day05.tasks
-  , Y2021.Day06.tasks
-  , Y2021.Day07.tasks
-  , Y2021.Day08.tasks
-  , Y2021.Day09.tasks
-  , Y2021.Day10.tasks
-  , Y2021.Day11.tasks
-  , Y2021.Day12.tasks
-  , Y2021.Day13.tasks
-  , Y2021.Day14.tasks
-  , Y2021.Day15.tasks
-  , Y2021.Day16.tasks
-  ]
+-- IMPORTS
 
 type TasksKey = (Integer, Int)
 
@@ -68,3 +35,5 @@ parseSelector _        = error "invalid selector"
 main = do
   selector <- parseSelector <$> getArgs
   traverse_ processTasks $ selectTasks selector allTasks
+
+-- ALL TASKS
