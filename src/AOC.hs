b426d55e37deefed55c07d6bf7486bacbbe29c06
Alexey Kotlyarov <a@koterpillar.com>
2021-12-23 20:54:25 +1100

    Extract AOC.SelectExample

--- src/AOC.hs
+++ src/AOC.hs
@@ -29,6 +29,7 @@ import           System.Directory            (doesFileExist)
 import           System.IO                   (isEOF)
 import           System.Timeout
 
+import AOC.SelectExample
 import           Miniparse
 import           Utils
 
@@ -82,38 +83,6 @@ getExample year day =
       simpleRequest $
       "https://adventofcode.com/" <> tshow year <> "/day/" <> tshow day
     pure $! selectExample year day $ codeBlocks page
-
-selectExample :: Integer -> Int -> [Text] -> Text
-selectExample _ _ [example] = example
-selectExample 2021 1 examples = head examples
-selectExample 2021 4 examples = head examples
-selectExample 2021 5 examples = head examples
-selectExample 2021 6 examples = head examples
-selectExample 2021 8 examples = examples !! 2
-selectExample 2021 9 examples = head examples
-selectExample 2021 10 examples = last examples
-selectExample 2021 11 examples = head examples
-selectExample 2021 12 examples = head examples
-selectExample 2021 13 examples = examples !! 1
-selectExample 2021 14 examples = head examples
-selectExample 2021 15 examples = head examples
-selectExample 2021 16 _ = "A0016C880162017C3686B18A3D4780"
-selectExample 2021 17 examples = head examples
-selectExample 2021 18 examples = examples !! 7
-selectExample 2021 19 examples = examples !! 5
-selectExample 2021 20 examples = head examples
-selectExample year day examples =
-  error $
-  "Cannot select from " <>
-  show (length examples) <>
-  " examples for year " <>
-  show year <>
-  " day " <>
-  show day <>
-  ":\n\n" <>
-  Text.unpack
-    (Text.unlines $
-     zipWith (\i e -> "--- item " <> tshow i <> " ---\n" <> e) [0 ..] examples)
 
 currentYear :: IO Integer
 currentYear = do
---
+++ src/AOC/SelectExample.hs
@@ -0,0 +1,37 @@
+module AOC.SelectExample where
+
+import qualified Data.Text as Text
+
+import           Utils
+
+selectExample :: Integer -> Int -> [Text] -> Text
+selectExample _ _ [example] = example
+selectExample 2021 1 examples = head examples
+selectExample 2021 4 examples = head examples
+selectExample 2021 5 examples = head examples
+selectExample 2021 6 examples = head examples
+selectExample 2021 8 examples = examples !! 2
+selectExample 2021 9 examples = head examples
+selectExample 2021 10 examples = last examples
+selectExample 2021 11 examples = head examples
+selectExample 2021 12 examples = head examples
+selectExample 2021 13 examples = examples !! 1
+selectExample 2021 14 examples = head examples
+selectExample 2021 15 examples = head examples
+selectExample 2021 16 _ = "A0016C880162017C3686B18A3D4780"
+selectExample 2021 17 examples = head examples
+selectExample 2021 18 examples = examples !! 7
+selectExample 2021 19 examples = examples !! 5
+selectExample 2021 20 examples = head examples
+selectExample year day examples =
+  error $
+  "Cannot select from " <>
+  show (length examples) <>
+  " examples for year " <>
+  show year <>
+  " day " <>
+  show day <>
+  ":\n\n" <>
+  Text.unpack
+    (Text.unlines $
+     zipWith (\i e -> "--- item " <> tshow i <> " ---\n" <> e) [0 ..] examples)
