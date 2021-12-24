e4502fe4c7a9c0e50a245b63db9cedab0b492d8e
Alexey Kotlyarov <a@koterpillar.com>
2021-12-24 14:09:16 +1100

    Day 24 skeleton

---
+++ src/Y2021/Day24.hs
@@ -0,0 +1,9 @@
+module Y2021.Day24 where
+
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+import           AOC
+import           Utils
+
+tasks = Tasks 2021 24 parse [Task part1 _]
