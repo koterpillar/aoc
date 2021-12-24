f24575660453c16e7d9b25114da74d0c75ad8deb
Alexey Kotlyarov <a@koterpillar.com>
2021-12-24 16:07:14 +1100

    Extract Counts

---
+++ src/Counts.hs
@@ -0,0 +1,60 @@
+module Counts where
+
+import qualified Data.Map as Map
+import           Utils
+
+newtype Counts a =
+  Counts
+    { getCounts :: Map a Int
+    }
+  deriving (Eq, Ord, Show)
+
+cpure :: Ord a => a -> Counts a
+cpure x = Counts $ Map.singleton x 1
+
+cFromListCount :: Ord a => [(a, Int)] -> Counts a
+cFromListCount = Counts . mapFromListSum
+
+cFromList :: Ord a => [a] -> Counts a
+cFromList = cFromListCount . flip zip (repeat 1)
+
+cToList :: Counts a -> [(a, Int)]
+cToList = Map.toList . getCounts
+
+cToSingle :: Show a => Counts a -> a
+cToSingle c =
+  case cToList c of
+    [(x, 1)] -> x
+    vs       -> error $ "cToSingle: " ++ show vs
+
+ctotal :: Counts a -> Int
+ctotal = sum . Map.elems . getCounts
+
+cmap :: Ord b => (a -> b) -> Counts a -> Counts b
+cmap f = Counts . Map.mapKeysWith (+) f . getCounts
+
+cmapf :: Ord b => Counts a -> (a -> b) -> Counts b
+cmapf = flip cmap
+
+cjoin :: Ord a => Counts (Counts a) -> Counts a
+cjoin = Counts . mapFromListSum . concatMap toListMul . cToList
+  where
+    toListMul (c, n) = [(v, m * n) | (v, m) <- cToList c]
+
+call :: (a -> Bool) -> Counts a -> Bool
+call f = all f . Map.keys . getCounts
+
+crights :: Ord b => Counts (Either a b) -> Maybe (Counts b)
+crights = fmap cFromListCount . traverse go . cToList
+  where
+    go (Right b, c) = Just (b, c)
+    go _            = Nothing
+
+cexamples :: Show a => Counts a -> String
+cexamples gs =
+  "Total: " <>
+  show (ctotal gs) <>
+  " Examples: " <> intercalate ", " (map show $ take 2 $ cToList gs)
+
+traceCounts :: Show a => Counts a -> Counts a
+traceCounts = traceF cexamples
--- src/Y2021/Day21.hs
+++ src/Y2021/Day21.hs
@@ -5,6 +5,7 @@ module Y2021.Day21 where
 import qualified Data.Map as Map
 
 import           AOC
+import           Counts
 import           Utils
 
 mod1 :: Int -> Int -> Int
@@ -16,53 +17,6 @@ mod1 a b =
 type Positions = [Int]
 
 type Player = Int
-
-newtype Counts a =
-  Counts
-    { getCounts :: Map a Int
-    }
-  deriving (Eq, Ord, Show)
-
-cpure :: Ord a => a -> Counts a
-cpure x = Counts $ Map.singleton x 1
-
-cFromListCount :: Ord a => [(a, Int)] -> Counts a
-cFromListCount = Counts . mapFromListSum
-
-cFromList :: Ord a => [a] -> Counts a
-cFromList = cFromListCount . flip zip (repeat 1)
-
-cToList :: Counts a -> [(a, Int)]
-cToList = Map.toList . getCounts
-
-cToSingle :: Show a => Counts a -> a
-cToSingle c =
-  case cToList c of
-    [(x, 1)] -> x
-    vs       -> error $ "cToSingle: " ++ show vs
-
-ctotal :: Counts a -> Int
-ctotal = sum . Map.elems . getCounts
-
-cmap :: Ord b => (a -> b) -> Counts a -> Counts b
-cmap f = Counts . Map.mapKeysWith (+) f . getCounts
-
-cmapf :: Ord b => Counts a -> (a -> b) -> Counts b
-cmapf = flip cmap
-
-cjoin :: Ord a => Counts (Counts a) -> Counts a
-cjoin = Counts . mapFromListSum . concatMap toListMul . cToList
-  where
-    toListMul (c, n) = [(v, m * n) | (v, m) <- cToList c]
-
-call :: (a -> Bool) -> Counts a -> Bool
-call f = all f . Map.keys . getCounts
-
-crights :: Ord b => Counts (Either a b) -> Maybe (Counts b)
-crights = fmap cFromListCount . traverse go . cToList
-  where
-    go (Right b, c) = Just (b, c)
-    go _            = Nothing
 
 data Dice
   = Deterministic Int
@@ -137,15 +91,6 @@ gameIsWin :: Game -> Bool
 gameIsWin = isJust . gameWin
 
 type Win = (Game, Player)
-
-cexamples :: Show a => Counts a -> String
-cexamples gs =
-  "Total: " <>
-  show (ctotal gs) <>
-  " Examples: " <> intercalate ", " (map show $ take 2 $ cToList gs)
-
-traceCounts :: Show a => Counts a -> Counts a
-traceCounts = traceF cexamples
 
 gamePlay :: Game -> Counts Win
 gamePlay = go . cpure
