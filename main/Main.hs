ee78c1a133835e32dd097dd7637a873e83e8e449
Alexey Kotlyarov <a@koterpillar.com>
2021-12-27 20:01:10 +1100

    Run last task in a year

--- main/Main.hs
+++ main/Main.hs
@@ -3,29 +3,38 @@ import qualified Data.Text          as Text
 
 import           System.Environment (getArgs)
 
-import           AllTasks
 import           AOC
+import           AllTasks
 import           Utils
 
 data Selector
   = All
   | Last
+  | LastInYear Integer
   | Year Integer
   | One Integer Int
   deriving (Show)
 
+plast :: [a] -> [a]
+plast = pure . last
+
+filterYear :: Integer -> [Tasks] -> [Tasks]
+filterYear y = filter $ \(Tasks ty _ _ _) -> ty == y
+
 selectTasks :: Selector -> [Tasks] -> [Tasks]
-selectTasks All ts       = ts
-selectTasks Last ts      = [last ts]
-selectTasks (Year y) ts  = [t | t@(Tasks ty _ _ _) <- ts, ty == y]
-selectTasks (One y d) ts = [t | t@(Tasks ty td _ _) <- ts, ty == y, td == d]
+selectTasks All            = id
+selectTasks Last           = plast
+selectTasks (LastInYear y) = plast . filterYear y
+selectTasks (Year y)       = filterYear y
+selectTasks (One y d)      = filter $ \(Tasks ty td _ _) -> ty == y && td == d
 
 parseSelector :: [String] -> Selector
-parseSelector []       = Last
-parseSelector ["all"]  = All
-parseSelector [ys]     = Year (read ys)
-parseSelector [ys, ds] = One (read ys) (read ds)
-parseSelector _        = error "invalid selector"
+parseSelector []          = Last
+parseSelector ["all"]     = All
+parseSelector [ys]        = LastInYear (read ys)
+parseSelector [ys, "all"] = Year (read ys)
+parseSelector [ys, ds]    = One (read ys) (read ds)
+parseSelector _           = error "invalid selector"
 
 main = do
   selector <- parseSelector <$> getArgs
