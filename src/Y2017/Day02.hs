c72b2545cf334956debebf0bec3b3786cacf295b
Alexey Kotlyarov <a@koterpillar.com>
2022-11-20 22:56:08 +1100

    2017 day 2

--- src/Y2017/Day02.hs
+++ src/Y2017/Day02.hs
@@ -1,6 +1,9 @@
-module Day02 where
+module Y2017.Day02 where
 
-import Control.Applicative
+import qualified Data.Text as Text
+
+import           AOC
+import           Utils
 
 lineChecksum :: [Int] -> Int
 lineChecksum lst = maximum lst - minimum lst
@@ -8,12 +11,17 @@ lineChecksum lst = maximum lst - minimum lst
 checksum :: [[Int]] -> Int
 checksum = sum . map lineChecksum
 
-parseLines :: String -> [[Int]]
-parseLines = map (map read . words) . lines
-
 divideDivisible :: [Int] -> Int
 divideDivisible lst =
   fst $
   head $
   filter (\(_, m) -> m == 0) $
   map (uncurry divMod) $ filter (uncurry (/=)) $ liftA2 (,) lst lst
+
+tasks =
+  Tasks
+    2017
+    2
+    (CodeBlock 0)
+    (linesP &** (wordsP &** integerP))
+    [Task checksum 18, TaskScraper (CodeBlock 1) (sum . map divideDivisible) 9]
