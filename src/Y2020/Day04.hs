20188efbfdf4f791dd171deb572258ad73c582dd
Alexey Kotlyarov <a@koterpillar.com>
2022-11-23 21:08:03 +1100

    2020 day 4 part 2

--- src/Y2020/Day04.hs
+++ src/Y2020/Day04.hs
@@ -21,6 +21,53 @@ isValid p = all (`Map.member` p) requiredElems
 
 requiredElems = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
 
-countValid = length . filter isValid
+fieldParser :: Text -> Parser Text a -> Parser Passport ()
+fieldParser k fp = lookupP k &* fp &* constP ()
 
-tasks = Tasks 2020 4 (CodeBlock 0) parsePassports [Task countValid 2]
+rangeP :: Ord k => k -> k -> Parser k k
+rangeP lo hi = filterP $ inRange lo hi
+
+yearParser :: Int -> Int -> Parser Text Int
+yearParser ymin ymax =
+  filterP (\t -> Text.length t == 4) &* integerP &* rangeP ymin ymax
+
+tspanP :: (Char -> Bool) -> Parser Text (Text, Text)
+tspanP = pureP . Text.span
+
+byr = fieldParser "byr" $ yearParser 1920 2002
+
+iyr = fieldParser "iyr" $ yearParser 2010 2020
+
+eyr = fieldParser "eyr" $ yearParser 2020 2030
+
+hgt =
+  fieldParser "hgt" $
+  tspanP isDigit &* (integerP &= idP) &* filterP (uncurry valid . swap)
+  where
+    valid "cm" = inRange 150 193
+    valid "in" = inRange 59 76
+    valid _    = const False
+
+hcl = fieldParser "hcl" $ tspanP (== '#') &* filterP (uncurry valid)
+  where
+    valid "#" t = Text.length t == 6 && Text.all isHexDigit t
+    valid _ _   = False
+    isHexDigit c = isDigit c || inRange 'a' 'f' c
+
+ecl =
+  fieldParser "ecl" $
+  filterP (`elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
+
+pid = fieldParser "pid" $ digitsP &* filterP (\t -> length t == 9)
+
+betterValid :: Passport -> Bool
+betterValid passport =
+  all isRight [runParse p passport | p <- [byr, iyr, eyr, hgt, hcl, ecl, pid]]
+
+tasks =
+  Tasks
+    2020
+    4
+    (CodeBlock 0)
+    parsePassports
+    [Task (countIf isValid) 2, Task (countIf betterValid) 2]
