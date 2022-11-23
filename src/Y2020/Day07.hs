2fcb040cd9bf8070c8c958a30c08e7bad02859e2
Alexey Kotlyarov <a@koterpillar.com>
2022-11-23 22:22:04 +1100

    2020 day 7

---
+++ src/Y2020/Day07.hs
@@ -0,0 +1,67 @@
+module Y2020.Day07 where
+
+import           Data.Function.Memoize
+import qualified Data.Map              as Map
+import qualified Data.Set              as Set
+import qualified Data.Text             as Text
+
+import           AOC
+import           Graph
+import           Utils
+
+type Bag = Text
+
+parseBag :: Parser Text Bag
+parseBag = pureP $ Text.replace " bag" "" . Text.replace " bags" ""
+
+parseInner :: Parser Text (Int, Bag)
+parseInner = tspanP isDigit &* (integerP &= pureP Text.tail &* parseBag)
+
+type Rule = (Bag, Set (Int, Bag))
+
+parseInners :: Parser Text (Set (Int, Bag))
+parseInners =
+  Set.fromList <$>
+  pureP (Text.replace "." "") &* tsplitP ", " &*
+  pureP (filter (/= "no other bags")) &**
+  parseInner
+
+parseRule :: Parser Text Rule
+parseRule = tsplitP " contain " &* (parseBag &+ parseInners)
+
+type Rules = Map Bag (Set (Int, Bag))
+
+parseRules :: Parser Text Rules
+parseRules = Map.fromList <$> linesP &** parseRule
+
+mkGraph :: Rules -> Graph Bag
+mkGraph = Map.map $ Set.map snd
+
+shinyGold :: Bag
+shinyGold = "shiny gold"
+
+findContainers :: Rules -> Int
+findContainers =
+  pred . length . reachableFrom shinyGold . reverseGraph . mkGraph
+
+findCost :: Rules -> Int
+findCost rules = pred $ findCost' rules shinyGold
+
+instance Memoizable Text where
+  memoize f t = memoize (f . Text.pack) (Text.unpack t)
+
+findCost' :: Rules -> Bag -> Int
+findCost' rules =
+  memoFix $ \memoFind bag ->
+    case Map.lookup bag rules of
+      Nothing -> error $ "no rule for " ++ show bag
+      Just inners ->
+        1 + sum [n * memoFind inner | (n, inner) <- Set.toList inners]
+
+tasks =
+  Tasks
+    2020
+    7
+    (CodeBlock 0)
+    parseRules
+    [Task findContainers 4, Task findCost 32]
