dd29d766a2cffb4d3637a4fcd7f3f8dca89fbf41
Alexey Kotlyarov <a@koterpillar.com>
2022-11-24 21:21:09 +1100

    2020 day 11 part 2

--- src/Y2020/Day11.hs
+++ src/Y2020/Day11.hs
@@ -23,18 +23,42 @@ type Grid = Grid2 Seat
 parseGrid :: Parser Text Grid
 parseGrid = fromMatrixG <$> linesP &** (charactersP &** choiceEBP ".L#")
 
-step :: Grid -> Grid
-step g =
-  Map.fromList $ map (\(p, v) -> (p, go v (countOccupied p))) $ Map.toList g
+countOccupied :: [Seat] -> Int
+countOccupied = countIf (== Occupied)
+
+occupiedNeighbors :: Grid -> Position2 -> Int
+occupiedNeighbors g p = countOccupied $ mapMaybe (`Map.lookup` g) $ adjacent8 p
+
+occupiedNeighbors2 g p = countOccupied $ mapMaybe (viewRay p) allDir8
   where
-    countOccupied p =
-      length $ filter (== Occupied) $ mapMaybe (`Map.lookup` g) $ adjacent8 p
+    viewRay :: Position2 -> Direction8 -> Maybe Seat
+    viewRay p d =
+      let p' = walk d p
+       in case Map.lookup p' g of
+            Just Floor -> viewRay p' d
+            x          -> x
+
+step :: (Grid -> Position2 -> Int) -> Int -> Grid -> Grid
+step occupiedNeighbors maxOccupied g =
+  Map.fromList $
+  map (\(p, v) -> (p, go v (occupiedNeighbors g p))) $ Map.toList g
+  where
     go Empty 0 = Occupied
     go Occupied n
-      | n >= 4 = Empty
+      | n >= maxOccupied = Empty
     go v _ = v
 
-stabilizedSeats :: Grid -> Int
-stabilizedSeats = countIf (== Occupied) . Map.elems . fixPoint step
+stabilizedSeats, stabilizedSeats2 :: Grid -> Int
+stabilizedSeats =
+  countOccupied . Map.elems . fixPoint (step occupiedNeighbors 4)
 
-tasks = Tasks 2020 11 (CodeBlock 1) parseGrid [Task stabilizedSeats 37]
+stabilizedSeats2 =
+  countOccupied . Map.elems . fixPoint (step occupiedNeighbors2 5)
+
+tasks =
+  Tasks
+    2020
+    11
+    (CodeBlock 1)
+    parseGrid
+    [Task stabilizedSeats 37, Task stabilizedSeats2 26]
