b50cb523b4295a8ee6da0ca30fdc42816c37a494
Alexey Kotlyarov <a@koterpillar.com>
2022-11-24 20:51:06 +1100

    2020 day 10 part 2

--- src/Y2020/Day10.hs
+++ src/Y2020/Day10.hs
@@ -9,4 +9,22 @@ oneAndThree adapters = countIf (== 1) stepups * countIf (== 3) stepups
     stepups = zipWith (-) (tail joltages) joltages
     joltages = 0 : sort adapters ++ [maximum adapters + 3]
 
-tasks = Tasks 2020 10 (CodeBlock 1) (linesP &** integerP) [Task oneAndThree 220]
+countChains :: [Int] -> Int
+countChains allAdapters = memoFix2 go 0 $ sort allAdapters
+  where
+    target = maximum allAdapters + 3
+    go :: (Int -> [Int] -> Int) -> Int -> [Int] -> Int
+    go _ v []
+      | target - v > 3 = 0
+      | otherwise = 1
+    go igo v (x:xs)
+      | x - v > 3 = 0
+      | otherwise = igo v xs + igo x xs
+
+tasks =
+  Tasks
+    2020
+    10
+    (CodeBlock 1)
+    (linesP &** integerP)
+    [Task oneAndThree 220, Task countChains 19208]
