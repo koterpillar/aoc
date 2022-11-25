08285608ab2d9d40cf51c7bb8564fa390774dc8e
Alexey Kotlyarov <a@koterpillar.com>
2022-11-25 16:39:13 +1100

    2020 day 14 part 2

--- src/Y2020/Day14.hs
+++ src/Y2020/Day14.hs
@@ -1,5 +1,7 @@
 module Y2020.Day14 where
 
+import           Control.Monad (zipWithM)
+
 import           Data.Bits
 
 import           AOC
@@ -63,5 +65,33 @@ applyInstruction (CPU mask mem) (SetMem addr val) =
 
 part1 = sum . cMem . foldl' applyInstruction (CPU undefined mempty)
 
+applyMask2 :: BitsMask -> Int -> [Int]
+applyMask2 (BitsMask m) = map bitsValue . zipWithM amb m . toBitStringPad
+  where
+    amb (Set O) b = [b]
+    amb (Set I) b = [I]
+    amb Ignore _  = [O, I]
+
+applyInstruction2 :: CPU -> Instruction -> CPU
+applyInstruction2 (CPU _ mem) (SetMask mask) = CPU mask mem
+applyInstruction2 (CPU mask mem) (SetMem addr val) =
+  CPU mask $
+  flip mappend mem $ mapFromList [(addr', val) | addr' <- applyMask2 mask addr]
+
+part2 = sum . cMem . foldl' applyInstruction2 (CPU undefined mempty)
+
 tasks =
-  Tasks 2020 14 (CodeBlock 0) (linesP &** parseInstruction) [Task part1 165]
+  Tasks
+    2020
+    14
+    (CodeBlock 0)
+    (linesP &** parseInstruction)
+    [ Task part1 165
+    , Assert
+        "amb"
+        [26, 27, 58, 59]
+        (applyMask2
+           (justParse parseBitMask "000000000000000000000000000000X1001X")
+           42)
+    , TaskScraper (CodeBlock 4) part2 208
+    ]
