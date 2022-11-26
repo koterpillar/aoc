1843cdd54e5627ea3e944914a19518fc4458758f
Alexey Kotlyarov <a@koterpillar.com>
2022-11-26 22:15:39 +1100

    don't parse expression, evaluate at once

--- src/Y2020/Day18.hs
+++ src/Y2020/Day18.hs
@@ -5,90 +5,53 @@ import           Control.Monad.State
 import           AOC
 import           Utils
 
-data Expr
-  = ENum Int
-  | EPlus Expr Expr
-  | EMult Expr Expr
-  deriving (Eq)
-
-instance Show Expr where
-  show (ENum n)      = show n
-  show (EPlus e1 e2) = showE "+" e1 e2
-  show (EMult e1 e2) = showE "*" e1 e2
-
-showE :: String -> Expr -> Expr -> String
-showE op e1 e2 = lp <> show e1 <> " " <> op <> " " <> show e2 <> rp
-  where
-    simple =
-      case e1 of
-        ENum _ -> True
-        _      -> False
-    lp =
-      if simple
-        then ""
-        else "("
-    rp =
-      if simple
-        then ""
-        else ")"
+data Op
+  = OPlus
+  | OMult
+  deriving (Eq, Show)
 
 data Token
   = TNum Int
-  | TPlus
-  | TMult
+  | TOp Op
   | TLParen
   | TRParen
   deriving (Eq, Show)
 
-flipToken :: Token -> Token
-flipToken TLParen = TRParen
-flipToken TRParen = TLParen
-flipToken t       = t
-
 tokenP :: Parser Text Token
 tokenP =
   (TNum <$> integerP) &|
-  choiceP [("+", TPlus), ("*", TMult), ("(", TLParen), (")", TRParen)]
+  choiceP [("+", TOp OPlus), ("*", TOp OMult), ("(", TLParen), (")", TRParen)]
 
-parseExpr :: Parser Text Expr
-parseExpr =
-  pureP (treplace "(" "( " . treplace ")" " )") &* wordsP &** tokenP &*
-  pureP (reverse . map flipToken) &* -- FIXME: to fix left-associativity
-  stateP exprSP
+parseExpr :: Parser Text [Token]
+parseExpr = pureP (treplace "(" "( " . treplace ")" " )") &* wordsP &** tokenP
 
 type SP a = StateParser [Token] a
 
-argSP :: SP Expr
-argSP =
+runSP = justParse . stateP
+
+arg :: SP Int -> SP Int
+arg ev =
   unconsSP_ >>= \case
-    TLParen -> do
-      e <- exprSP
-      ensureUnconsSP_ TRParen
-      pure e
-    TNum v -> pure $ ENum v
-    a -> failSP $ "argSP: " ++ show a
+    TLParen -> ev <* ensureUnconsSP_ TRParen
+    TNum v  -> pure v
+    a       -> failSP $ "arg: " ++ show a
 
-exprSP :: SP Expr
-exprSP = do
-  a1 <- argSP
-  unconsSP >>= \case
-    Nothing -> pure a1
-    Just TRParen -> do
-      putBackSP TRParen
-      pure a1
-    Just TPlus -> do
-      a2 <- exprSP
-      pure $ EPlus a1 a2
-    Just TMult -> do
-      a2 <- exprSP
-      pure $ EMult a1 a2
-    Just a -> failSP $ "exprSP: " ++ show a
+opApply :: Op -> Int -> Int -> Int
+opApply OPlus = (+)
+opApply OMult = (*)
 
-evaluate :: Expr -> Int
-evaluate (ENum x)    = x
-evaluate (EPlus x y) = evaluate x + evaluate y
-evaluate (EMult x y) = evaluate x * evaluate y
+evaluate1 :: SP Int
+evaluate1 = go 0 OPlus
+  where
+    go a op = do
+      b <- arg evaluate1
+      let c = opApply op a b
+      unconsSP >>= \case
+        Nothing        -> pure c
+        Just (TOp op2) -> go c op2
+        Just TRParen   -> putBackSP TRParen *> pure c
+        Just a         -> failSP $ "evaluate1: " ++ show a
 
-part1 = sum . map evaluate
+part1 = sum . map (runSP evaluate1)
 
 tasks = Tasks 2020 18 (InlineCode 4) (linesP &** parseExpr) [Task part1 12240]
