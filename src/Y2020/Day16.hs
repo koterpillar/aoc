35a1f737ce73621474183f243ac794c0f2a9876f
Alexey Kotlyarov <a@koterpillar.com>
2022-11-26 21:21:53 +1100

    2020 day 16 part 2

--- src/Y2020/Day16.hs
+++ src/Y2020/Day16.hs
@@ -52,18 +52,22 @@ invalid1 Notes {nFields = rs} (Ticket fs) =
 part1 n = sum $ concatMap (invalid1 n) (nTickets n)
 
 nValidTickets :: Notes -> [Ticket]
-nValidTickets n = filter (null . invalid1 n) (nTickets n)
+nValidTickets n = filter (null . invalid1 n) $ nTickets n
 
 data Search =
   Search
-    { sFound      :: Map Text Int
-    , sRemRules   :: Map Text IntSet
-    , sRemIndices :: Map Int IntSet
+    { sFound   :: Map Text Int
+    , sRemPoss :: Map Text IntSet
     }
   deriving (Ord, Eq, Show)
 
 instance Hashable Search where
-  hashWithSalt x (Search a b c) = hashWithSalt x (a, b, c)
+  hashWithSalt x (Search a b) = hashWithSalt x (a, b)
+
+pick :: Ord k => Map k IntSet -> ((k, IntSet), Map k IntSet)
+pick m = ((k, vs), Map.delete k m)
+  where
+    (k, vs) = minimumBy (compare `on` (IntSet.size . snd)) $ Map.toList m
 
 match :: Notes -> Map Text Int
 match n =
@@ -71,22 +75,16 @@ match n =
   traceShowId $
   lastE "empty route found" $
   fromJustE "no route found" $
-  aStar
-    moves
-    (const $ const 1)
-    (length . sRemIndices)
-    (null . sRemIndices)
-    start
+  aStar moves (const $ const 1) (length . sRemPoss) (null . sRemPoss) start
   where
-    start = Search Map.empty (ruleSets n) (ticketSets n)
+    start = Search mempty $ traceShowId $ rulesPossibilities n
     moves Search {..} =
       hashSetFromList $ do
-        (r, range) <- Map.toList sRemRules
-        (i, values) <- Map.toList sRemIndices
-        guard $ IntSet.isSubsetOf values range
-        let sFound' = Map.insert r i sFound
-        pure $
-          Search sFound' (Map.delete r sRemRules) (Map.delete i sRemIndices)
+        let ((r, poss), rp1) = pick sRemPoss
+        i <- IntSet.toList poss
+        let rp2 = Map.map (IntSet.delete i) rp1
+        guard $ all (not . IntSet.null) rp2
+        pure $ Search (Map.insert r i sFound) rp2
 
 ticketSets :: Notes -> Map Int IntSet
 ticketSets =
@@ -97,6 +95,15 @@ ruleSets :: Notes -> Map Text IntSet
 ruleSets =
   Map.map (mconcat . map (IntSet.fromList . uncurry enumFromTo) . unRule) .
   nFields
+
+rulePossibilities :: Map Int IntSet -> IntSet -> IntSet
+rulePossibilities tsets range =
+  IntSet.fromList $
+  map fst $
+  filter (\(idx, values) -> IntSet.isSubsetOf values range) $ Map.toList tsets
+
+rulesPossibilities :: Notes -> Map Text IntSet
+rulesPossibilities n = Map.map (rulePossibilities (ticketSets n)) (ruleSets n)
 
 part2 n =
   product $
