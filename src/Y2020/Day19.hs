a3084ffcaeb725cb99426e969ab03b5c68651e30
Alexey Kotlyarov <a@koterpillar.com>
2022-11-28 13:09:55 +1100

    Investigate rules

--- src/Y2020/Day19.hs
+++ src/Y2020/Day19.hs
@@ -10,6 +10,8 @@ import           Utils
 data Rule
   = RChar Char
   | RRecurse [[Int]]
+  | RMany Int
+  | RManyNested Int Int
   deriving (Eq, Show)
 
 ruleP :: Parser Text Rule
@@ -39,24 +41,91 @@ getRule rules idx =
 matcher :: Rules -> Int -> StateParser Message ()
 matcher rules idx = go (getRule rules idx)
   where
-    go (RChar c)           = unconsSP_ >>= guard . (== c)
-    go (RRecurse [r1])     = goSeq r1
-    go (RRecurse [r1, r2]) = goSeq r1 <|> goSeq r2
-    go x                   = error $ show x
+    go (RChar c) = unconsSP_ >>= guard . (== c)
+    go (RRecurse rs) = foldr1 (<|>) $ map goSeq rs
+    go (RMany i) = void $ many (matcher rules i)
+    go (RManyNested a b) = do
+      as <- length <$> many (matcher rules a)
+      replicateM_ as (matcher rules b)
     goSeq = traverse_ $ matcher rules
 
 matches :: Rules -> Message -> Bool
 matches rules = isRight . runParse (stateP $ matcher rules 0)
 
+allMatching :: Rules -> Int -> Set Message
+allMatching rules idx = go (getRule rules idx)
+  where
+    go (RChar c)     = Set.singleton [c]
+    go (RRecurse rs) = mconcat $ map goSeq rs
+    go x             = error $ show x
+    goSeq =
+      foldr1 (\a b -> Set.map (uncurry (++)) $ Set.cartesianProduct a b) .
+      map (allMatching rules)
+
+countValid :: Input -> Int
 countValid (rs, msgs) = countIf (matches rs) msgs
 
-fixups =
-  mapFromList
-    [(8, RRecurse [[42], [42, 8]]), (11, RRecurse [[42, 31], [42, 11, 31]])]
+fixups :: Rules
+fixups = mapFromList [(8, RMany 42), (11, RManyNested 42 31)]
 
+recursionIsEasy :: Input -> Set Message
+recursionIsEasy (rules, _) = a `Set.intersection` b
+  where
+    a = allMatching rules 31
+    b = allMatching rules 42
+
+-- 42 is this
+easySet42 :: Set Message
+easySet42 =
+  Set.fromList
+    [ "aaaaa"
+    , "aaaab"
+    , "aaaba"
+    , "aaabb"
+    , "aabbb"
+    , "ababa"
+    , "abbbb"
+    , "baaaa"
+    , "baabb"
+    , "babbb"
+    , "bbaaa"
+    , "bbaab"
+    , "bbabb"
+    , "bbbab"
+    , "bbbba"
+    , "bbbbb"
+    ]
+
+-- 31 is this
+easySet31 :: Set Message
+easySet31 =
+  Set.fromList
+    [ "aabaa"
+    , "aabab"
+    , "aabba"
+    , "abaaa"
+    , "abaab"
+    , "ababb"
+    , "abbaa"
+    , "abbab"
+    , "abbba"
+    , "baaab"
+    , "baaba"
+    , "babaa"
+    , "babab"
+    , "babba"
+    , "bbaba"
+    , "bbbaa"
+    ]
+
+doFixups :: Rules -> Rules
 doFixups rs = fixups <> rs
 
+countValidFixups :: Input -> Int
 countValidFixups (rs, msgs) = countValid (doFixups rs, msgs)
+
+startRule :: Input -> Rule
+startRule = fromJust . Map.lookup 0 . fst
 
 tasks =
   Tasks
@@ -65,39 +134,13 @@ tasks =
     (CodeBlock 4)
     parser
     [ Task countValid 3
+    , AssertExample "all matching 31" easySet31 (flip allMatching 31 . fst)
+    , AssertExample "all matching 42" easySet42 (flip allMatching 42 . fst)
+    , Task recursionIsEasy Set.empty
+    , Task startRule (RRecurse [[8, 11]])
     , AssertExample
         "message 3 matches"
         True
         (flip matches "babbbbaabbbbbabbbbbbaabaaabaaa" . doFixups . fst)
     , Task countValidFixups 12
     ]
-
-data Rule2
-  = R2Char Char
-  | R2Alt [Rule2]
-  | R2Seq [Rule2]
-  deriving (Eq, Show)
-
-to2 :: Rules -> Rule2
-to2 rules = go Set.empty 0
-  where
-    go :: Set Int -> Int -> Rule2
-    go seen i
-      | i `Set.member` seen = error $ "loop detected: " ++ show (seen, i)
-      | otherwise =
-        case r of
-          RChar c    -> R2Char c
-          RRecurse r -> R2Alt $ map (R2Seq . map (go seen')) r
-      where
-        r = fromJust $ Map.lookup i rules
-        seen' = Set.insert i seen
-
-simplify :: Rule2 -> Rule2
-simplify (R2Seq [r]) = r
-simplify (R2Alt [r]) = r
-simplify (R2Alt rs)  = R2Alt (map simplify rs)
-simplify (R2Seq rs)  = R2Seq (map simplify rs)
-simplify r           = r
-
-convert :: Rules -> Rule2
-convert = iterateSettle simplify . to2
