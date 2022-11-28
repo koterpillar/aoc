f3ec954c5ef1d5dd69306a38075a0bb4ec2213aa
Alexey Kotlyarov <a@koterpillar.com>
2022-11-28 22:09:01 +1100

    2020 day 20 part 1 parse

---
+++ src/Y2020/Day20.hs
@@ -0,0 +1,64 @@
+module Y2020.Day20 where
+
+import qualified Data.Map  as Map
+import qualified Data.Text as Text
+
+import           AOC
+import           Grid
+import           Utils
+
+type Grid = Grid2 ()
+
+data Tile =
+  Tile
+    { tid   :: Int
+    , tgrid :: Grid
+    }
+  deriving (Show)
+
+parseTiles :: Parser Text [Tile]
+parseTiles = lineGroupsP &** parseTile
+
+parseTile :: Parser [Text] Tile
+parseTile =
+  uncurry Tile <$>
+  unconsP &*
+  (pureP (Text.replace "Tile " "" . Text.replace ":" "") &* integerP &=
+   pureP Text.unlines &*
+   dotGridP)
+
+type Edge = [Bool]
+
+-- Oops, we'll have 8 edges because of flips, but can't use them all at once!
+edges :: Grid -> [Edge]
+edges g = map getEdge addresses
+  where
+    (Position2 x1 y1, Position2 x2 y2) = boundsG g
+    addresses =
+      withReverse $
+      map
+        (uncurry $ zipWith Position2)
+        [ ([x1 .. x2], repeat y1)
+        , ([x1 .. x2], repeat y2)
+        , (repeat x1, [y1 .. y2])
+        , (repeat x2, [y1 .. y2])
+        ]
+    withReverse = concatMap (\a -> [a, reverse a])
+    getEdge = map (`Map.member` g)
+
+allEdges :: [Tile] -> Map Edge [Int]
+allEdges tiles =
+  Map.fromListWith
+    (++)
+    [(edge, [tid]) | Tile {..} <- tiles, edge <- edges tgrid]
+
+cornerIds :: [Tile] -> Int
+cornerIds tiles = product corners
+  where
+    ae = allEdges tiles
+    unmatched = Map.filter ((== 1) . length) ae
+    unmatchedWithCount =
+      traceShowId $ mapFromListCount $ join $ Map.elems unmatched
+    corners = Map.keys $ Map.filter (== 4) unmatchedWithCount -- 4 because of flips
+
+tasks = Tasks 2020 20 (CodeBlock 0) parseTiles [Task cornerIds 20899048083289]
