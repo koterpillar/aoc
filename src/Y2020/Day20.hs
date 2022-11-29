4267897ffc01550f514acdea4fd809ab87525ddf
Alexey Kotlyarov <a@koterpillar.com>
2022-11-29 18:04:17 +1100

    WIP

--- src/Y2020/Day20.hs
+++ src/Y2020/Day20.hs
@@ -29,22 +29,31 @@ parseTile =
 
 type Edge = [Bool]
 
+flips :: Grid -> [Grid]
+flips g =
+  [ Map.mapKeys (a . b . c) g
+  | a <- [id, flipX]
+  , b <- [id, flipY]
+  , c <- [id, flipXY]
+  ]
+  where
+    flipX (Position2 x y) = Position2 (xmax + xmin - x) y
+    flipY (Position2 x y) = Position2 x (ymax + ymin - y)
+    flipXY (Position2 x y) = Position2 y x
+    (Position2 xmin ymin, Position2 xmax ymax) = boundsG g
+
+edge :: Direction4 -> Grid -> Edge
+edge d g = [Map.member p g | p <- points d]
+  where
+    (Position2 xmin ymin, Position2 xmax ymax) = boundsG g
+    points E = [Position2 xmax y | y <- [ymin .. ymax]]
+    points N = [Position2 x ymin | x <- [xmin .. xmax]]
+    points W = [Position2 xmin y | y <- [ymin .. ymax]]
+    points S = [Position2 x ymax | x <- [xmin .. xmax]]
+
 -- Oops, we'll have 8 edges because of flips, but can't use them all at once!
 edges :: Grid -> [Edge]
-edges g = map getEdge addresses
-  where
-    (Position2 x1 y1, Position2 x2 y2) = boundsG g
-    addresses =
-      withReverse $
-      map
-        (uncurry $ zipWith Position2)
-        [ ([x1 .. x2], repeat y1)
-        , ([x1 .. x2], repeat y2)
-        , (repeat x1, [y1 .. y2])
-        , (repeat x2, [y1 .. y2])
-        ]
-    withReverse = concatMap (\a -> [a, reverse a])
-    getEdge = map (`Map.member` g)
+edges = map (edge N) . flips
 
 allEdges :: [Tile] -> Map Edge [Int]
 allEdges tiles =
@@ -61,4 +70,44 @@ cornerIds tiles = product corners
       traceShowId $ mapFromListCount $ join $ Map.elems unmatched
     corners = Map.keys $ Map.filter (== 4) unmatchedWithCount -- 4 because of flips
 
-tasks = Tasks 2020 20 (CodeBlock 0) parseTiles [Task cornerIds 20899048083289]
+merge :: [Tile] -> Grid
+merge = error "not implemented"
+
+exampleMerged :: Text
+exampleMerged =
+  Text.replace "." (Text.singleton middleDot) $
+  Text.unlines
+    [ ".#.#..#.##...#.##..#####"
+    , "###....#.#....#..#......"
+    , "##.##.###.#.#..######..."
+    , "###.#####...#.#####.#..#"
+    , "##.#....#.##.####...#.##"
+    , "...########.#....#####.#"
+    , "....#..#...##..#.#.###.."
+    , ".####...#..#.....#......"
+    , "#..#.##..#..###.#.##...."
+    , "#.####..#.####.#.#.###.."
+    , "###.#.#...#.######.#..##"
+    , "#.####....##..########.#"
+    , "##..##.#...#...#.#.#.#.."
+    , "...#..#..#.#.##..###.###"
+    , ".#.#....#.##.#...###.##."
+    , "###.#...#..#.##.######.."
+    , ".#.#.###.##.##.#..#.##.."
+    , ".####.###.#...###.#..#.#"
+    , "..#.#..#..#.#.#.####.###"
+    , "#..####...#.#.#.###.###."
+    , "#####..#####...###....##"
+    , "#.##..#..#...#..####...#"
+    , ".#.###..##..##..####.##."
+    , "...###...##...#...#..###"
+    ]
+
+tasks :: Tasks
+tasks =
+  Tasks
+    2020
+    20
+    (CodeBlock 0)
+    parseTiles
+    [Task cornerIds 20899048083289, Task (displayG . merge) exampleMerged]
