6df9d9fb547e1e22a83672fadfee6a2222aa5efc
Alexey Kotlyarov <a@koterpillar.com>
2022-12-01 20:29:57 +1100

    Add traceAO and traceAO2

--- src/Utils/Trace.hs
+++ src/Utils/Trace.hs
@@ -26,3 +26,19 @@ ttraceF f a = ttrace (f a) a
 
 ttraceM :: Monad m => Text -> m ()
 ttraceM = traceM . Text.unpack
+
+prependShow :: Show a => String -> a -> String
+prependShow msg a = msg ++ " " ++ show a
+
+traceO :: Show o => String -> o -> o
+traceO msg = traceF (prependShow $ msg ++ ">")
+
+traceAO :: (Show a, Show o) => String -> (a -> o) -> a -> o
+traceAO msg fn = traceO msg . fn . traceF (prependShow $ msg ++ "<")
+
+traceAO2 ::
+     (Show a1, Show a2, Show o) => String -> (a1 -> a2 -> o) -> a1 -> a2 -> o
+traceAO2 msg fn =
+  curry $
+  traceO msg .
+  uncurry fn . traceF (\(a1, a2) -> msg ++ "< " ++ show a1 ++ show a2)
