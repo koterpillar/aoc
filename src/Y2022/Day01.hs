6fd6f9e22c042d9168d9f515b11b773e4ebcb400
Alexey Kotlyarov <a@koterpillar.com>
2022-12-01 17:03:56 +1100

    Day 1

---
+++ src/Y2022/Day01.hs
@@ -0,0 +1,15 @@
+module Y2022.Day01 where
+
+import           AOC
+import           Utils
+
+parser :: Parser Text [[Int]]
+parser = lineGroupsP &** traverseP integerP
+
+part1 :: [[Int]] -> Int
+part1 = sum . maximumOn sum
+
+part2 :: [[Int]] -> Int
+part2 = sum . join . take 3 . sortOn (negate . sum)
+
+tasks = Tasks 2022 1 (CodeBlock 0) parser [Task part1 24000, Task part2 45000]
