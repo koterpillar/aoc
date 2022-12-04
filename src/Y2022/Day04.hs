c724db928b40dc3c2b3c5132bbd37e20817db269
Alexey Kotlyarov <a@koterpillar.com>
2022-12-04 16:08:54 +1100

    Day 04

---
+++ src/Y2022/Day04.hs
@@ -0,0 +1,26 @@
+module Y2022.Day04 where
+
+import           AOC
+import           Utils
+
+type Range = (Int, Int)
+
+parser :: Parser Text [(Range, Range)]
+parser = linesP &** tsplitP "," &* rangeP &+ rangeP
+
+rangeP :: Parser Text Range
+rangeP = tsplitP "-" &* integerP &+ integerP
+
+subsumes :: Range -> Range -> Bool
+subsumes (a, b) (c, d) =
+  inRange a b c && inRange a b d || inRange c d a && inRange c d b
+
+overlaps :: Range -> Range -> Bool
+overlaps (a, b) (c, d) =
+  inRange a b c || inRange a b d || inRange c d a || inRange c d b
+
+part1 = countIf (uncurry subsumes)
+
+part2 = countIf (uncurry overlaps)
+
+tasks = Tasks 2022 4 (CodeBlock 0) parser [Task part1 2, Task part2 4]
