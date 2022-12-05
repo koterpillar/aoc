03186fe1bf082763a6b58f7828613e6ed44f5405
Alexey Kotlyarov <a@koterpillar.com>
2022-12-05 16:55:41 +1100

    Day 05

---
+++ src/Y2022/Day05.hs
@@ -0,0 +1,78 @@
+module Y2022.Day05 where
+
+import qualified Data.Map  as Map
+import qualified Data.Set  as Set
+import qualified Data.Text as Text
+
+import           AOC
+import           Utils
+
+type Stack = [Char]
+
+type Stacks = Map Int Stack
+
+data Instruction =
+  Instruction
+    { iFrom  :: Int
+    , iTo    :: Int
+    , iCount :: Int
+    }
+  deriving (Eq, Show)
+
+type Input = (Stacks, [Instruction])
+
+parseInstruction :: Parser Text Instruction
+parseInstruction =
+  wordsP &* pureP (\[_, x, _, y, _, z] -> (x, (y, z))) &*
+  (integerP &= (integerP &= integerP)) &*
+  pureP makeInstruction
+  where
+    makeInstruction (c, (f, t)) = Instruction f t c
+
+stacksP :: Parser [Text] Stacks
+stacksP =
+  Map.fromList . zip [1 ..] . map catMaybes . transpose <$>
+  pureP dropLast &* traverseP listsP
+
+dropLast ls = take (length ls - 1) ls
+
+listsP :: Parser Text [Maybe Char]
+listsP =
+  pureP (map (maybespace . headE "listsP" . tail) . chunksOf 4 . Text.unpack)
+  where
+    maybespace ' ' = Nothing
+    maybespace c   = Just c
+
+parser :: Parser Text Input
+parser = lineGroupsP &* stacksP &+ traverseP parseInstruction
+
+applyInstruction :: Instruction -> Stacks -> Stacks
+applyInstruction i@(Instruction from to count) =
+  iterateN count $ \s0 ->
+    let ff = s0 Map.! from
+        f = headE "move from empty" ff
+        f1 = tail ff
+        s1 = Map.insert from f1 s0
+        s2 = Map.adjust (f :) to s1
+     in s2
+
+applyInstruction2 :: Instruction -> Stacks -> Stacks
+applyInstruction2 i@(Instruction from to count) s0 =
+  let ff = s0 Map.! from
+      fs = take count ff
+      f1 = drop count ff
+      s1 = Map.insert from f1 s0
+      s2 = Map.adjust (fs ++) to s1
+   in s2
+
+solve :: (Instruction -> Stacks -> Stacks) -> Input -> String
+solve app (stacks, instructions) =
+  map head $ Map.elems $ foldl' (flip app) stacks instructions
+
+tasks =
+  Tasks
+    2022
+    5
+    (CodeBlock 0)
+    parser
+    [Task (solve applyInstruction) "CMZ", Task (solve applyInstruction2) "MCD"]
