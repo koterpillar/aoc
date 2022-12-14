9b1ff4324f9d47461978c27c564ac1e7a4090483
Alexey Kotlyarov <a@koterpillar.com>
2022-12-14 13:43:15 +1100

    Split Miniparse

---
+++ src/Miniparse/State.hs
@@ -0,0 +1,61 @@
+module Miniparse.State where
+
+import           Control.Monad.State
+
+import           Data.Char
+
+import           Text.Read           (readEither)
+
+import           Miniparse.Base
+
+type StateParser src dest = StateT src (Either String) dest
+
+stateP ::
+     (Eq src, Monoid src, Show src) => StateParser src dest -> Parser src dest
+stateP p =
+  Parser $
+  runStateT p >=> \(result, remainder) ->
+    if remainder == mempty
+      then Right result
+      else Left $ "unconsumed remainder: " ++ show remainder
+
+unconsSP :: StateParser [a] (Maybe a)
+unconsSP =
+  state $ \case
+    []     -> (Nothing, [])
+    (x:xs) -> (Just x, xs)
+
+unconsSP_ :: StateParser [a] a
+unconsSP_ = unconsSP >>= maybe (failSP "unconsSP_: end of input") pure
+
+ensureUnconsSP :: (Eq a, Show a) => Maybe a -> StateParser [a] ()
+ensureUnconsSP expected =
+  unconsSP >>= \actual ->
+    if actual == expected
+      then pure ()
+      else failSP $ "expected " <> show expected <> " but got " <> show actual
+
+ensureUnconsSP_ :: (Eq a, Show a) => a -> StateParser [a] ()
+ensureUnconsSP_ = ensureUnconsSP . Just
+
+putBackSP :: a -> StateParser [a] ()
+putBackSP x = modify (x :)
+
+failSP :: String -> StateParser src dest
+failSP = lift . Left
+
+readSP :: Read a => String -> StateParser src a
+readSP = lift . readEither
+
+unstateSP :: Monoid src => Parser src dest -> StateParser src dest
+unstateSP p = StateT $ fmap (, mempty) . runParse p
+
+naturalSP :: StateParser String Int
+naturalSP = go >>= readSP
+  where
+    go =
+      unconsSP >>= \case
+        Nothing -> pure []
+        (Just c)
+          | isDigit c -> (c :) <$> go
+          | otherwise -> putBackSP c >> pure []
