2d1e0847318cba34c70ae754b04b8a54c8c1383f
Alexey Kotlyarov <a@koterpillar.com>
2022-12-15 17:28:19 +1100

    Day 15 part 2

--- src/Y2022/Day15.hs
+++ src/Y2022/Day15.hs
@@ -29,24 +29,18 @@ mkGrid :: Input -> Grid
 mkGrid = Map.fromList . concatMap (\(p1, p2) -> [(p1, Sensor), (p2, Beacon)])
 
 parser :: Parser Text Input
-parser =
-  linesP &** wordsP &*
-  pureP
-    (map (terase "x=" . terase "y=" . terase "," . terase ":") .
-     filter (Text.isInfixOf "=")) &*
-  pps &*
-  (pp &= pp)
+parser = linesP &** tsplitP ":" &* (part &+ part)
   where
-    pps = pureP $ \[x1, y1, x2, y2] -> ((x1, y1), (x2, y2))
-    pp = uncurry Position2 <$> (integerP &= integerP)
+    part =
+      pureP (terase "x=" . terase "y=" . terase "," . terase ":") &* wordsP &*
+      pureP (takeEnd 2) &*
+      pairPWith Position2 integerP integerP
 
-empties :: (Int -> Bool) -> Position2 -> Position2 -> Set Position2
-empties yfilter sensor@(Position2 sx sy) beacon =
+empties :: Int -> Position2 -> Position2 -> Set Position2
+empties y sensor@(Position2 sx sy) beacon =
   Set.fromList
     [ p
-    | y <- [ymin .. ymax]
-    , yfilter y
-    , x <- [xmin .. xmax]
+    | x <- [xmin .. xmax]
     , let p = Position2 x y
     , manhattanDistance sensor p <= d
     , p /= beacon
@@ -58,18 +52,47 @@ empties yfilter sensor@(Position2 sx sy) beacon =
     ymin = sy - d
     ymax = sy + d
 
+isExample :: Input -> Bool
 isExample input = fst (head input) == Position2 2 18
 
-part1 input =
-  length $
-  Map.filterWithKey (\(Position2 _ y) v -> y == filterY && v == Empty) g'
+part1 input = length $ Map.filter (== Empty) g'
   where
     g = mkGrid input
     filterY =
       if isExample input
         then 10
         else 2000000
-    es = Set.unions $ map (uncurry $ empties (== filterY)) input
+    es = Set.unions $ map (uncurry $ empties filterY) input
     g' = Map.union g $ Map.fromSet (const Empty) es
 
-tasks = Tasks 2022 15 (CodeBlock 0) parser [Task part1 26]
+tuningFreq :: Position2 -> Int
+tuningFreq (Position2 x y) = x * 4000000 + y
+
+signs :: [Int -> Int -> Int]
+signs = [(+), (-)]
+
+borders :: Position2 -> Position2 -> [Position2]
+borders sensor@(Position2 x y) beacon =
+  [ Position2 (x `s1` o) (y `s2` (d - o))
+  | o <- [0 .. d]
+  , s1 <- signs
+  , s2 <- signs
+  ]
+  where
+    d = manhattanDistance sensor beacon + 1
+
+part2 :: Input -> Int
+part2 input =
+  tuningFreq $
+  fromSingleE "filtered" $ nubOrd $ filter (\p -> all (invisibleTo p) input) bs
+  where
+    mx =
+      if isExample input
+        then 20
+        else 4000000
+    inRangeP (Position2 x y) = inRange 0 mx x && inRange 0 mx y
+    bs = concatMap (filter inRangeP . uncurry borders) input
+    invisibleTo p (sensor, beacon) =
+      manhattanDistance p sensor > manhattanDistance beacon sensor
+
+tasks = Tasks 2022 15 (CodeBlock 0) parser [Task part1 26, Task part2 56000011]
