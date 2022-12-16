2c3630c9dcd2fa87d9c882a9131fbbeb86831c06
Alexey Kotlyarov <a@koterpillar.com>
2022-12-16 23:46:50 +1100

    Binary tree to check whether to insert

--- src/Y2022/Day16.hs
+++ src/Y2022/Day16.hs
@@ -97,35 +97,62 @@ vInit minutes workers vMap = VState {..}
     vTotalTime = minutes
     vReleased = 0
 
-vNotBetter :: VState -> VState -> Bool
-vNotBetter a b =
-  vPosition a == vPosition b &&
-  Set.isSubsetOf (vOpenK b) (vOpenK a) && vTotalFlow a >= vTotalFlow b
-
 newtype Layers =
-  Layers (Map (VKey, Set VKey) VState)
+  Layers (Map VKey KTree)
+
+data KTree
+  = Leaf VState
+  | Stmp
+  | Node VKey KTree KTree
+  deriving (Eq, Ord, Show)
+
+kempty :: KTree
+kempty = Stmp
+
+ktoList :: KTree -> [VState]
+ktoList (Leaf v)     = [v]
+ktoList (Node _ l r) = ktoList l ++ ktoList r
+
+kinsert :: VState -> KTree -> KTree
+kinsert v Stmp = Leaf v
+kinsert v1 (Leaf v2) =
+  case sampleDiff (vOpenK v1) (vOpenK v2) of
+    Just k -> Node k (Leaf v2) (Leaf v1)
+    Nothing ->
+      case sampleDiff (vOpenK v2) (vOpenK v1) of
+        Just k  -> Node k (Leaf v1) (Leaf v2)
+        Nothing -> Leaf v1
+kinsert v (Node k l r)
+  | Set.member k (vOpenK v) = Node k l (kinsert v r)
+  | otherwise = Node k (kinsert v l) r
+
+-- | Returns an element that is present in first but not the second set, if one exists.
+sampleDiff :: Ord a => Set a -> Set a -> Maybe a
+sampleDiff a b = Set.lookupMin (Set.difference a b)
 
 layers0 :: Layers
 layers0 = Layers mempty
 
 unLayers :: Layers -> [VState]
-unLayers (Layers ls) = toList ls
+unLayers (Layers ls) = concatMap ktoList $ toList ls
 
 layersSize :: Layers -> Int
 layersSize (Layers ls) = length ls
 
+kWorthy :: VState -> KTree -> Bool
+kWorthy _ Stmp = True
+kWorthy b (Leaf a) = vTotalFlow b > vTotalFlow a
+kWorthy b (Node k l r)
+  | Set.member k (vOpenK b) = kWorthy b l && kWorthy b r
+  | otherwise = kWorthy b l
+
 keep :: Layers -> VState -> Bool
-keep (Layers ls) v = not $ any (`vNotBetter` v) os
-  where
-    os =
-      catMaybes
-        [ Map.lookup (vPosition v, o) ls
-        | o <- Set.toList (Set.powerSet (vOpenK v))
-        ]
+keep (Layers ls) v = kWorthy v $ fromMaybe Stmp $ Map.lookup (vPosition v) ls
 
 layerAdd :: VState -> Layers -> Layers
 layerAdd v l@(Layers ls)
-  | keep l v = Layers $ Map.insert (vPosition v, vOpenK v) v ls
+  | keep l v =
+    Layers $ Map.alter (Just . kinsert v . fromMaybe kempty) (vPosition v) ls
   | otherwise = l
 
 layerAddMany :: Layers -> [VState] -> Layers
@@ -145,8 +172,8 @@ lGo l ls =
   where
     l1 = traceF (prependShow "min" . vMinute . head) l
     ns0 = traceF (prependShow "raw" . length) $ concatMap vMoves l1
-    ns2 = traceF (prependShow "cmp" . length) $ compact ns0
-    ns = ns2
+    ns1 = traceF (prependShow "cmp" . length) $ compact ns0
+    ns = ns1
 
 start :: Int -> Int -> Input -> Int
 start minutes workers input =
