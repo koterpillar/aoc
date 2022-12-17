8b46e8a491e9a9bc43b0a6b528b53bf50be66745
Alexey Kotlyarov <a@koterpillar.com>
2022-12-17 20:49:22 +1100

    Naive DFS

--- src/Y2022/Day16.hs
+++ src/Y2022/Day16.hs
@@ -2,9 +2,11 @@
 
 module Y2022.Day16 where
 
-import qualified Data.Map  as Map
-import qualified Data.Set  as Set
-import qualified Data.Text as Text
+import           Control.Monad.State.Strict
+
+import qualified Data.Map                   as Map
+import qualified Data.Set                   as Set
+import qualified Data.Text                  as Text
 
 import           AOC
 import           Path
@@ -108,94 +110,33 @@ vInit minutes workers vMap = VState {..}
     vTotalTime = minutes
     vReleased = 0
 
-newtype Layers =
-  Layers (Map [VKey] KTree)
+type Seen = Set ([VKey], Set VKey)
 
-data KTree
-  = Leaf VState
-  | Stmp
-  | Node VKey KTree KTree
-  deriving (Eq, Ord, Show)
+initSeen :: Seen
+initSeen = mempty
 
-kempty :: KTree
-kempty = Stmp
+checkMarkSeen :: VState -> State Seen Bool
+checkMarkSeen s = do
+  let k = (vPositions s, vOpenK s)
+  gets (Set.member k) >>= \case
+    True -> pure False
+    False -> do
+      traceM $ prependShow "new" s
+      modify' (Set.insert k)
+      pure True
 
-ktoList :: KTree -> [VState]
-ktoList Stmp         = []
-ktoList (Leaf v)     = [v]
-ktoList (Node _ l r) = ktoList l ++ ktoList r
-
-ksize :: KTree -> Int
-ksize = length . ktoList
-
-kinsert :: VState -> KTree -> KTree
-kinsert v Stmp = Leaf v
-kinsert v1 (Leaf v2) =
-  case sampleDiff (vOpenK v1) (vOpenK v2) of
-    Just k -> Node k (Leaf v2) (Leaf v1)
-    Nothing ->
-      case sampleDiff (vOpenK v2) (vOpenK v1) of
-        Just k  -> Node k (Leaf v1) (Leaf v2)
-        Nothing -> Leaf v1
-kinsert v (Node k l r)
-  | Set.member k (vOpenK v) = Node k l (kinsert v r)
-  | otherwise = Node k (kinsert v l) r
-
--- | Returns an element that is present in first but not the second set, if one exists.
-sampleDiff :: Ord a => Set a -> Set a -> Maybe a
-sampleDiff a b = Set.lookupMin (Set.difference a b)
-
-layers0 :: Layers
-layers0 = Layers mempty
-
-unLayers :: Layers -> [VState]
-unLayers (Layers ls) = concatMap ktoList $ toList ls
-
-layersSize :: Layers -> Int
-layersSize (Layers ls) = sum $ map ksize $ toList ls
-
-kWorthy :: VState -> KTree -> Bool
-kWorthy _ Stmp = True
-kWorthy b (Leaf a) = vTotalFlow b > vTotalFlow a
-kWorthy b (Node k l r)
-  | Set.member k (vOpenK b) = kWorthy b l && kWorthy b r
-  | otherwise = kWorthy b l
-
-keep :: Layers -> VState -> Bool
-keep (Layers ls) v = maybe True (kWorthy v) $ Map.lookup (vPositions v) ls
-
-layerAdd :: VState -> Layers -> Layers
-layerAdd v l@(Layers ls)
-  | keep l v =
-    Layers $ Map.alter (Just . kinsert v . fromMaybe kempty) (vPositions v) ls
-  | otherwise = l
-
-layerAddMany :: Layers -> [VState] -> Layers
-layerAddMany = foldr layerAdd
-
-layerMk :: [VState] -> Layers
-layerMk = layerAddMany layers0
-
-compact :: [VState] -> [VState]
-compact = unLayers . layerMk
-
-lGo :: [VState] -> Layers -> Layers
-lGo l ls =
-  if null ns
-    then ls
-    else lGo ns $ traceF (prependShow "lrs" . layersSize) $ layerAddMany ls l
-  where
-    l1 = traceF (prependShow "min" . vMinute . head) l
-    ns0 = traceF (prependShow "raw" . length) $ concatMap vMoves l1
-    ns1 = traceF (prependShow "cmp" . length) $ compact ns0
-    ns = ns1
+dfs :: VState -> State Seen [VState]
+dfs a =
+  checkMarkSeen a >>= \case
+    False -> pure []
+    True  -> fmap ((a :) . concat) <$> traverse dfs $ vMoves a
 
 start :: Int -> Int -> Input -> Int
 start minutes workers input =
   vTotalFlow $
   head $
   traceF (unlines . map show) $
-  minimumsOn vMinute $ maximumsOn vTotalFlow $ unLayers $ lGo [st] layers0
+  minimumsOn vMinute $ maximumsOn vTotalFlow $ evalState (dfs st) initSeen
   where
     st = vInit minutes workers input
 
