08042da4fb4dc03ed1989163e36ec049db87ccd9
Alexey Kotlyarov <a@koterpillar.com>
2022-12-17 19:30:36 +1100

    Day 17

--- src/Y2022/Day17.hs
+++ src/Y2022/Day17.hs
@@ -43,9 +43,6 @@ rocks =
     , [Position2 x y | x <- [0 .. 1], y <- [0 .. 1]]
     ]
 
-initChamber :: Grid
-initChamber = mkgrid [Position2 x 0 | x <- [1 .. 7]]
-
 putInChamber :: Rock -> State St ()
 putInChamber rock = do
   let (Position2 rxmin rymin, Position2 rxmax rymax) = boundsG rock
@@ -61,9 +58,16 @@ data St =
     , stWindInd :: Int
     , stChamber :: Grid
     , stRock    :: Maybe Rock
-    , stRockInd :: Int
+    , stDropped :: Int
     , stOffset  :: Int
+    , stPrev    :: Map StKey St
     }
+  deriving (Show)
+
+type StKey = (Int, Grid, Int)
+
+stKey :: St -> StKey
+stKey St {..} = (stWindInd, stChamber, stDropped `mod` length rocks)
 
 data GI
   = R
@@ -86,10 +90,11 @@ stInit :: [Direction4] -> St
 stInit stWind = St {..}
   where
     stWindInd = 0
-    stChamber = initChamber
+    stChamber = mkgrid [Position2 x 0 | x <- [1 .. 7]]
     stRock = Nothing
-    stRockInd = 0
+    stDropped = 0
     stOffset = 0
+    stPrev = mempty
 
 stHeight :: St -> Int
 stHeight St {..} = -ymin + stOffset
@@ -135,14 +140,14 @@ dropRockGo = do
                    (stChamber s)
              }
 
-dropRock :: Int -> State St ()
-dropRock n = do
-  when (n `mod` 10000 == 0) $ traceShowM n
-  when (n `mod` 10 == 0) optimize
-  r <- (rocks !!) <$> gets stRockInd
+dropRock :: State St ()
+dropRock = do
+  n <- gets stDropped
+  when (n `mod` 20 == 0) optimize
+  let r = rocks !! (n `mod` length rocks)
   putInChamber r
-  modify $ \s -> s {stRockInd = succMod (length rocks) (stRockInd s)}
   dropRockGo
+  modify $ \s -> s {stDropped = succ n}
 
 probe :: Grid -> Int -> Int -> Int -> Maybe Int
 probe g ymin ymax x =
@@ -157,20 +162,32 @@ optimize = do
     let g' =
           Map.mapKeys (\(Position2 x y) -> Position2 x (y - cutoff)) $
           Map.filterWithKey (\(Position2 _ y) _ -> y <= cutoff) g
-    traceM $
-      "optimized " ++
-      show (length g) ++
-      " to " ++
-      show (length g') ++
-      " pts " ++ show (sum $ map (\(Position2 x y) -> x + y) $ Map.keys g')
     modify $ \s -> s {stChamber = g', stOffset = stOffset s - cutoff}
 
+dropRocks :: Int -> State St ()
+dropRocks 0 = pure ()
+dropRocks n = do
+  dropRock
+  k <- gets stKey
+  v <- gets (Map.lookup k . stPrev)
+  case v of
+    Nothing -> do
+      modify $ \s -> s {stPrev = Map.insert (stKey s) s (stPrev s)}
+      dropRocks $ pred n
+    Just s0 -> do
+      s1 <- get
+      let period = stDropped s1 - stDropped s0
+      let growth = stHeight s1 - stHeight s0
+      let skip = n `div` period
+      modify $ \s ->
+        s
+          { stDropped = stDropped s + skip * period
+          , stOffset = stOffset s + skip * growth
+          }
+      dropRocks $ pred n - skip * period
+
 start :: Int -> [Direction4] -> Int
-start n input = stHeight st1
-  where
-    st0 = stInit $ traceF (prependShow "input length" . length) input
-    act = traverse dropRock [1 .. n]
-    st1 = execState act st0
+start n = stHeight . execState (dropRocks n) . stInit
 
 tasks =
   Tasks
