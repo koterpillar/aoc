bf755fe8cc2858d97453369d312b7c120265e25e
Alexey Kotlyarov <a@koterpillar.com>
2022-12-18 21:32:53 +1100

    Day 16: goal oriented

--- src/Y2022/Day16.hs
+++ src/Y2022/Day16.hs
@@ -14,9 +14,18 @@ import           AOC
 import           Path
 import           Utils
 
-import           Y2022.Lattice
+newtype VKey =
+  VKey Text
+  deriving (Eq, Ord)
 
-type VKey = Text
+instance Hashable VKey where
+  hashWithSalt s (VKey k) = hashWithSalt s k
+
+vAA :: VKey
+vAA = VKey "AA"
+
+instance Show VKey where
+  show (VKey k) = Text.unpack k
 
 data VData =
   VData
@@ -35,14 +44,15 @@ iValves = mapFilterValues $ (> 0) . vFlow
 
 parser :: Parser Text Input
 parser =
-  Map.fromList <$> linesP &** wordsP &* pureP tail &* unconsP &* (idP &= vdp)
+  Map.fromList <$>
+  linesP &** wordsP &* pureP tail &* unconsP &* (pureP VKey &= vdp)
   where
     vdp =
       pureP (drop 2) &*
       (uncurry mkVData <$>
        unconsP &*
        ((pureP (terase "rate=" . terase ";") &* integerP) &=
-        (pureP (drop 4 . map (terase ",")) &* idP)))
+        (pureP (drop 4 . map (terase ",")) &** pureP VKey)))
 
 -- | Cut out the given vertex, connecting its two neighbors directly instead.
 mExclude :: VKey -> ((Int, VKey), (Int, VKey)) -> Input -> Input
@@ -66,172 +76,71 @@ mExcludeOneZero input = do
     listToMaybe
       [ (b, ((da, a), (dc, c)))
       | (b, VData {vNext = [(da, a), (dc, c)], vFlow = 0}) <- Map.toList input
-      , b /= "AA"
+      , b /= vAA
       ]
   pure $ mExclude b ds input
 
 mExcludeZero :: Input -> Input
 mExcludeZero = iterateMaybeL mExcludeOneZero
 
-data VPosition
-  = VAt VKey
-  | VEnRoute Int VKey
+mPath :: Input -> VKey -> VKey -> Int
+mPath m from to =
+  fst $
+  lastE "mPath" $
+  fromJustE "mPath" $
+  aStar moves (subtract `on` fst) (const 0) ((== to) . snd) (0, from)
+  where
+    moves (d, k) = [(d + d', k') | (d', k') <- vNext $ mapLookupE "mPath" k m]
+
+mValves :: Input -> [VKey]
+mValves = mapFilterValues ((/= 0) . vFlow)
+
+type Order = [VKey]
+
+mOrders :: Input -> [Order]
+mOrders = permutations . mValves
+
+data Event =
+  Event
+    { eTime  :: Int
+    , eValve :: VKey
+    }
   deriving (Eq, Ord, Show)
 
-vEnRoute :: Int -> VKey -> VPosition
-vEnRoute 0 = VAt
-vEnRoute i
-  | i < 0 = error $ "vEnRoute: negative time: " ++ show i
-  | otherwise = VEnRoute i
+mExecute :: Input -> Order -> [Event]
+mExecute m = mExecuteFrom m vAA
 
-vDue :: VPosition -> Int
-vDue (VAt _)        = 0
-vDue (VEnRoute i _) = i
-
-vSubtract :: Int -> VPosition -> VPosition
-vSubtract 0 p = p
-vSubtract i p@(VAt _) =
-  error $ "Can't subtract " ++ show i ++ " from " ++ show p
-vSubtract i (VEnRoute j k) = vEnRoute (j - i) k
-
-data VState =
-  VState
-    { vMap       :: Input
-    , vPositions :: [VPosition]
-    , vOpenK     :: Set VKey
-    , vMinute    :: Int
-    , vTotalTime :: Int
-    , vReleased  :: Int
-    }
-  deriving (Eq, Ord)
-
-vAt :: VState -> VKey -> VData
-vAt VState {..} k = mapLookupE "vAt" k vMap
-
-vPosition :: Int -> VState -> VPosition
-vPosition i VState {..} = vPositions !! i
-
-instance Show VState where
-  show s =
-    "T" ++
-    show (vMinute s) ++
-    " at " ++
-    show (vPositions s) ++
-    " open " ++
-    show (Set.toList $ vOpenK s) ++
-    " released " ++
-    show (vReleased s) ++
-    " current " ++ show (vCurrentFlow s) ++ " total " ++ show (vTotalFlow s)
-
-vOpen :: VState -> [VData]
-vOpen s = map (vAt s) $ Set.toList (vOpenK s)
-
-vCurrentFlow :: VState -> Int
-vCurrentFlow = sum . map vFlow . vOpen
-
-vTotalFlow :: VState -> Int
-vTotalFlow s = vReleased s + (vTotalTime s - vMinute s) * vCurrentFlow s
-
-vIsOpen :: VKey -> VState -> Bool
-vIsOpen k s = k `Set.member` vOpenK s
-
-vTurn :: Int -> VState -> Maybe VState
-vTurn i s =
-  case vPosition i s of
-    VEnRoute {} -> Nothing
-    VAt k       -> go k
+mExecuteFrom :: Input -> VKey -> Order -> [Event]
+mExecuteFrom _ _ [] = []
+mExecuteFrom m a (b:bs) = ev : mExecuteFrom m b bs
   where
-    go k
-      | vIsOpen k s = Nothing
-      | vFlow (vAt s k) == 0 = Nothing
-      | otherwise = Just s {vOpenK = Set.insert k (vOpenK s)}
+    ev = Event (p + 1) b
+    p = mPath m a b
 
-vWalk :: Int -> VState -> VPosition -> VState
-vWalk i s k = s {vPositions = sset i k $ vPositions s}
-
-vReachable :: Int -> VState -> [VPosition]
-vReachable i s =
-  case vPosition i s of
-    VEnRoute i k -> [vEnRoute (i - 1) k]
-    VAt k        -> [vEnRoute (i - 1) k' | (i, k') <- vNext $ vAt s k]
-
-vIndices :: VState -> [Int]
-vIndices s = [0 .. length (vPositions s) - 1]
-
-vMoveOne :: Int -> VState -> [VState]
-vMoveOne i s = maybeToList (vTurn i s) ++ map (vWalk i s) (vReachable i s)
-
-vMoves :: VState -> [VState]
-vMoves s0
-  | vMinute s0 >= vTotalTime s0 = []
-  | otherwise = vWarp . tick <$> foldrM vMoveOne s0 (vIndices s0)
+mTally :: Input -> Int -> [Event] -> Int
+mTally m limit = go 0 0
   where
-    tick s_ = s_ {vMinute = succ $ vMinute s_, vReleased = flow}
-    flow = vReleased s0 + vCurrentFlow s0
+    go _ _ [] = 0
+    go f t1 (Event t2 k:es)
+      | t2 > limit = 0
+      | otherwise = f * dt + go (f + kf) t2 es
+      where
+        dt = t2 - t1
+        kf = vFlow $ mapLookupE "mTally" k m
 
-vWarp :: VState -> VState
-vWarp s =
-  s
-    { vMinute = vMinute s + t
-    , vPositions = ps'
-    , vReleased = vReleased s + t * vCurrentFlow s
-    }
+mOrderScore :: Input -> Int -> Order -> Int
+mOrderScore m limit o = mTally m limit $ mExecute m o
+
+start :: Int -> Input -> Int
+start minutes input0 = maximum $ map score orders
   where
-    ps = vPositions s
-    t = minimum $ map vDue ps
-    ps' = map (vSubtract t) ps
+    input = mExcludeZero input0
+    orders = traceF (prependShow "orders length" . length) $ mOrders input
+    score = mOrderScore input minutes
 
-vInit :: Int -> Int -> Input -> VState
-vInit minutes workers vMap = VState {..}
-  where
-    vPositions = replicate workers $ VAt "AA"
-    vPrevs = replicate workers Nothing
-    vOpenK = mempty
-    vMinute = 0
-    vTotalTime = minutes
-    vReleased = 0
+part1 = start 30
 
-instance Positionable VState where
-  latticePosition s =
-    map valve (iValves $ vMap s) ++ [60 - vMinute s, vReleased s]
-    where
-      valve k =
-        if vIsOpen k s
-          then 1
-          else 0
-
-type Seen = Map [VPosition] Lattice
-
-initSeen :: Seen
-initSeen = mempty
-
-checkMarkSeen :: VState -> State Seen Bool
-checkMarkSeen a = do
-  let k = vPositions a
-  bs <- gets $ fromMaybe latticeEmpty . Map.lookup k
-  case latticeInsert bs a of
-    Nothing -> pure False
-    Just bs' -> do
-      traceM $ prependShow "improvement" a
-      modify' (Map.insert k bs')
-      pure True
-
-dfs :: VState -> State Seen [VState]
-dfs a =
-  checkMarkSeen a >>= \case
-    False -> pure []
-    True  -> fmap ((a :) . concat) <$> traverse dfs $ vMoves a
-
-start :: Int -> Int -> Input -> Int
-start minutes workers input =
-  vTotalFlow $ traceShowId $ maximumOn vTotalFlow $ evalState (dfs st) initSeen
-  where
-    st = vInit minutes workers input'
-    input' = mExcludeZero input
-
-part1 = start 30 1
-
-part2 = start 26 2
+part2 = error "not implemented" -- start 26 2
 
 -- on my data the right answer to part 2 is between 2722 and 2922
 tasks = Tasks 2022 16 (CodeBlock 0) parser [Task part1 1651, Task part2 1707]
