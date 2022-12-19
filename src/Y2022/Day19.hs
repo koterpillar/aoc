818b45e6720d1a2253c9fd40bd02f0602222b7af
Alexey Kotlyarov <a@koterpillar.com>
2022-12-19 16:40:51 +1100

    Naive search

--- src/Y2022/Day19.hs
+++ src/Y2022/Day19.hs
@@ -1,10 +1,11 @@
 module Y2022.Day19 where
 
-import qualified Data.Map  as Map
-import qualified Data.Set  as Set
-import qualified Data.Text as Text
+import qualified Data.Map      as Map
+import qualified Data.Set      as Set
+import qualified Data.Text     as Text
 
 import           AOC
+import           Data.Foldable (foldrM)
 import           Utils
 
 data Material
@@ -17,24 +18,24 @@ data Material
 materialP :: Parser Text Material
 materialP = choiceEBP ["ore", "clay", "obsidian", "geode"]
 
-type Cost = Map Material Int
+type Inventory = Map Material Int
 
-costP :: Parser Text Cost
-costP =
+inventoryP :: Parser Text Inventory
+inventoryP =
   Map.fromList <$>
   tsplitP " and " &** (swap <$> wordsP &* integerP &+ materialP)
 
 data Blueprint =
   Blueprint
     { bID    :: Int
-    , bCosts :: Map Material Cost
+    , bCosts :: Map Material Inventory
     }
   deriving (Eq, Ord, Show)
 
-recipeP :: Parser Text (Material, Cost)
+recipeP :: Parser Text (Material, Inventory)
 recipeP =
   tsplitP " robot costs " &* (pureP (terase "Each ") &* materialP) &+
-  (pureP (terase ".") &* costP)
+  (pureP (terase ".") &* inventoryP)
 
 blueprintP :: Parser Text Blueprint
 blueprintP =
@@ -47,6 +48,51 @@ parser =
   pureP (treplace "\n  " " ") &* linesP &* pureP (filter $ not . Text.null) &**
   blueprintP
 
-part1 = error . show
+maxGeodes :: Blueprint -> Int
+maxGeodes b = go b 24 stInit
+
+data St =
+  St
+    { stRobots    :: Inventory
+    , stResources :: Inventory
+    }
+  deriving (Eq, Ord, Show)
+
+stInit :: St
+stInit = St {..}
+  where
+    stRobots = Map.singleton Ore 1
+    stResources = Map.fromList $ zip enumerate $ repeat 0
+
+stTick :: St -> St
+stTick st = st {stResources = newResources}
+  where
+    newResources = Map.mapWithKey incr $ stResources st
+    incr m n = n + fromMaybe 0 (Map.lookup m $ stRobots st)
+
+stTake :: Material -> Int -> St -> Maybe St
+stTake m q s =
+  if available >= q
+    then Just $ s {stResources = Map.adjust (subtract q) m res}
+    else Nothing
+  where
+    res = stResources s
+    available = fromMaybe 0 $ Map.lookup m res
+
+stConstruct :: Blueprint -> Material -> St -> Maybe St
+stConstruct b m st = foldrM (uncurry stTake) st1 $ Map.toList bom
+  where
+    st1 = st {stRobots = Map.insertWith (+) m 1 $ stRobots st}
+    bom = mapLookupE "bom" m $ bCosts b
+
+go :: Blueprint -> Int -> St -> Int
+go _ 0 St {..} = fromMaybe 0 $ Map.lookup Geode stResources
+go b time st =
+  traceShow (time, st) $
+  maximum $
+  map (go b (time - 1) . stTick) $
+  catMaybes [stConstruct b robot st | robot <- reverse enumerate] ++ [st]
+
+part1 bps = sum [bID bp * maxGeodes bp | bp <- bps]
 
 tasks = Tasks 2022 19 (CodeBlock 0) parser [Task part1 33]
