be2f38271cad1694adf0d2b1fdaadbd40f4c4ef0
Alexey Kotlyarov <a@koterpillar.com>
2022-12-20 17:10:11 +1100

    Fix iterateN and iterateNL

--- src/Utils/Iterate.hs
+++ src/Utils/Iterate.hs
@@ -9,31 +9,26 @@ type ItFn a = (a -> a) -> a -> [a]
 type ItFnL a = (a -> a) -> a -> a
 
 iterateN :: Int -> ItFn a
-iterateN 0 _ v = [v]
-iterateN n fn v =
-  let !u = iterateN (n - 1) fn v
-   in v : u
+iterateN n f = take (succ n) . iterate' f
 
 iterateNL :: Int -> ItFnL a
 iterateNL n f = last . iterateN n f
 
 iterateWhile :: (a -> Bool) -> ItFn a
 iterateWhile c fn v
-  | c v =
-    let v' = fn v
-     in v : iterateWhile c fn v'
+  | c v = v : iterateWhile c fn (fn v)
   | otherwise = []
 
 iterateWhileL :: (a -> Bool) -> ItFnL a
 iterateWhileL c f = last . iterateWhile c f
 
 iterateSettle :: Eq a => ItFn a
-iterateSettle fn v =
-  if v == v'
-    then [v]
-    else v : iterateSettle fn v'
+iterateSettle fn v = v : vs
   where
     v' = fn v
+    vs
+      | v == v' = []
+      | otherwise = iterateSettle fn v'
 
 iterateSettleL :: Eq a => ItFnL a
 iterateSettleL f = last . iterateSettle f
