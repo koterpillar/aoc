84dd497b1ccde747d524ce9db3f8ce5fc067b70f
Alexey Kotlyarov <a@koterpillar.com>
2023-12-01 16:37:15 +1100

    2023 day 01

---
+++ src/Y2023/Day01.hs
@@ -0,0 +1,49 @@
+module Y2023.Day01 where
+
+import qualified Data.Map  as Map
+import qualified Data.Set  as Set
+import qualified Data.Text as Text
+
+import           AOC
+import           Utils
+
+parser :: Parser Text [[Char]]
+parser = linesP &** charactersP
+
+decimal :: [Int] -> Int
+decimal ns = head ns * 10 + last ns
+
+part1 :: [[Char]] -> Int
+part1 = sum . map (decimal . mapMaybe naiveConvert)
+
+naiveConvert :: Char -> Maybe Int
+naiveConvert c
+  | isDigit c = Just $ ord c - ord '0'
+  | otherwise = Nothing
+
+extractChars :: String -> [Int]
+extractChars = mapMaybe extractChar . join . map tails . inits
+
+extractChar :: [Char] -> Maybe Int
+extractChar "one"   = Just 1
+extractChar "two"   = Just 2
+extractChar "three" = Just 3
+extractChar "four"  = Just 4
+extractChar "five"  = Just 5
+extractChar "six"   = Just 6
+extractChar "seven" = Just 7
+extractChar "eight" = Just 8
+extractChar "nine"  = Just 9
+extractChar [c]     = naiveConvert c
+extractChar _       = Nothing
+
+part2 :: [String] -> Int
+part2 = sum . map (decimal . extractChars)
+
+tasks =
+  Tasks
+    2023
+    1
+    (CodeBlock 0)
+    parser
+    [Task part1 142, TaskScraper (CodeBlock 1) part2 281]
