4914e7b7820b8691db34de21e692329ca509885a
Alexey Kotlyarov <a@koterpillar.com>
2023-12-09 06:17:11 +0100

    2023 day 9

---
+++ src/Y2023/Day09.hs
@@ -0,0 +1,27 @@
+module Y2023.Day09 where
+
+import qualified Data.Map  as Map
+import qualified Data.Set  as Set
+import qualified Data.Text as Text
+
+import           AOC
+import           Utils
+
+parser = linesP &** integersP " "
+
+diff :: [Int] -> [Int]
+diff xs = zipWith (-) (tail xs) xs
+
+predict :: [Int] -> Int
+predict xs
+  | all (== 0) xs = 0
+  | otherwise = last xs + y'
+  where
+    ys = diff xs
+    y' = predict ys
+
+part1 = sum . map predict
+
+part2 = part1 . map reverse
+
+tasks = Tasks 2023 9 (CodeBlock 0) parser [Task part1 114, Task part2 2]
