8cb77a1653c793bf9748f2739eab9533b2b9dab9
Alexey Kotlyarov <a@koterpillar.com>
2023-12-12 13:37:38 +0100

    Calculate group counts

--- src/Y2023/Day12.hs
+++ src/Y2023/Day12.hs
@@ -1,10 +1,11 @@
 module Y2023.Day12 where
 
-import qualified Data.Map  as Map
-import qualified Data.Set  as Set
-import qualified Data.Text as Text
+import qualified Data.Map            as Map
+import qualified Data.Set            as Set
+import qualified Data.Text           as Text
 
 import           AOC
+import           GHC.IO.Handle.Types (Handle__)
 import           Utils
 
 data Pos1
@@ -44,37 +45,44 @@ groupP = map (map pos1) . filter (not . null) . splitOn [Op1]
 groupL :: Line -> (Row, Counts)
 groupL = first groupP
 
+type MF1 a b = (a -> b) -> a -> b
+
 type MF2 a b c = (a -> b -> c) -> a -> b -> c
 
-nullableGroup :: Group -> Bool
-nullableGroup = all (== Uk)
+groupCounts :: Group -> Map Counts Int
+groupCounts = memoFix gc1
 
-matchGroup :: Group -> Int -> [Maybe Group]
-matchGroup = memoFix2 matchGroup1
-
-matchGroup1 :: MF2 Group Int [Maybe Group]
-matchGroup1 _ [] n = []
-matchGroup1 _ g@(Da:_) n
-  | length g < n = []
-  | length g <= n + 1 = [Nothing]
-  | otherwise = [Just gr | d == Uk]
+gc1 :: MF1 Group (Map Counts Int)
+gc1 _ [] = Map.singleton [] 1
+gc1 f (Uk:ps) = f (Da : ps) `mapSum` f ps
+gc1 f p@(Da:_)
+  | null rest = Map.singleton [must] 1
+  | otherwise =
+    Map.mapKeys (must :) (f $ tail rest) `mapSum`
+    f (take must p ++ (Da : tail rest))
   where
-    (gl, d:gr) = splitAt n g
-matchGroup1 f (Uk:gs) n = f (Da : gs) n ++ f gs n
+    must = length $ takeWhile (== Da) p
+    rest = drop must p
+    restR = f $ tail rest
+
+addToFirst :: Num a => a -> [a] -> Maybe [a]
+addToFirst a (b:c) = Just $ (a + b) : c
+addToFirst _ _     = Nothing
 
 possibilities :: Line -> Int
-possibilities = uncurry (memoFix2 poss1) . groupL
-
-poss1 :: MF2 Row Counts Int
-poss1 _ gs []
-  | all nullableGroup gs = 1
-  | otherwise = 0
-poss1 _ [] _ = 0
-poss1 f (g:gs) (c:cs) =
-  sum [f (maybeToList g' ++ gs) cs | g' <- g1s] +
-  sum [f gs (c : cs) | nullableGroup g]
+possibilities (p1s, cs) = poss (groupP p1s) cs
   where
-    g1s = matchGroup g c
+    poss = poss1 poss
+
+poss1 :: MF2 [Group] Counts Int
+poss1 _ [] [] = 1
+poss1 _ [] _ = 0
+poss1 f (g:gs) cs =
+  sum $ do
+    (c, p) <- Map.toList $ groupCounts g
+    guard $ c `isPrefixOf` cs
+    let cs' = drop (length c) cs
+    pure $ p * f gs cs'
 
 part1 :: [Line] -> Int
 part1 = sum . map possibilities
@@ -88,8 +96,45 @@ tasks =
     12
     (CodeBlock 1)
     parser
-    [ AssertExample "second last line" 4 (possibilities . head . tail . reverse)
-    , AssertExample "each line" [1, 4, 1, 1, 4, 10] (map possibilities)
+    [ Assert "gc1 ?##?" (Map.fromList [([2], 1), ([3], 2), ([4], 1)]) $
+      groupCounts [Uk, Da, Da, Uk]
+    , Assert
+        "gc1 ???"
+        (Map.fromList [([], 1), ([1], 3), ([1, 1], 1), ([2], 2), ([3], 1)]) $
+      groupCounts $ replicate 3 Uk
+    , Assert
+        "gc1 ????"
+        (Map.fromList
+           [ ([], 1)
+           , ([1], 4)
+           , ([1, 1], 3)
+           , ([1, 2], 1)
+           , ([2], 3)
+           , ([2, 1], 1)
+           , ([3], 2)
+           , ([4], 1)
+           ]) $
+      groupCounts $ replicate 4 Uk
+    , Assert
+        "gc1 ?????"
+        (Map.fromList
+           [ ([], 1)
+           , ([1], 5)
+           , ([1, 1], 6)
+           , ([1, 1, 1], 1)
+           , ([1, 2], 3)
+           , ([1, 3], 1)
+           , ([2], 4)
+           , ([2, 1], 3)
+           , ([2, 2], 1)
+           , ([3], 3)
+           , ([3, 1], 1)
+           , ([4], 2)
+           , ([5], 1)
+           ]) $
+      groupCounts $ replicate 5 Uk
+    , AssertExample "second last line" 4 $ possibilities . head . tail . reverse
+    , AssertExample "each line" [1, 4, 1, 1, 4, 10] $ map possibilities
     , Task part1 21
     , Task part2 525152
     ]
