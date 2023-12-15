9b2323aeade769b3ab9b90a016e3472f0965103b
Alexey Kotlyarov <a@koterpillar.com>
2023-12-15 22:05:09 +1100

    part 2

--- src/Y2023/Day13.hs
+++ src/Y2023/Day13.hs
@@ -10,40 +10,51 @@ import           Utils
 
 type Grid = Grid2 ()
 
+transposeG :: Grid -> Grid
+transposeG = Map.mapKeys (\(Position2 x y) -> Position2 y x)
+
 parser :: Parser Text [Grid]
 parser = lineGroupsP &** (pureP Text.unlines &* dotGridP)
 
-listToMaybeE []  = Nothing
-listToMaybeE [x] = Just x
-listToMaybeE _   = error "listToMaybeE: more than one element"
-
-findReflectionV :: Grid -> Maybe Int
-findReflectionV g = find isReflection [succ xmin .. xmax]
+xrange :: Grid -> [Int]
+xrange g = [succ xmin .. xmax]
   where
     (Position2 xmin _, Position2 xmax _) = boundsG g
-    ps = Map.keys g
-    isReflection x = all (containsFlipped x) ps
-    containsFlipped xc (Position2 x y) =
-      xf < xmin || xf > xmax || Map.member (Position2 xf y) g
+
+findReflectionV :: Grid -> Maybe Int
+findReflectionV g = find (null . smudgesV g) (xrange g)
+
+smudgesV :: Grid -> Int -> [Position2]
+smudgesV g x = filter (flippedMissing x) (Map.keys g)
+  where
+    (Position2 xmin _, Position2 xmax _) = boundsG g
+    flippedMissing xc (Position2 x y) =
+      xf >= xmin && xf <= xmax && Map.notMember (Position2 xf y) g
       where
         xf = 2 * xc - 1 - x
 
-findReflectionH :: Grid -> Maybe Int
-findReflectionH =
-  findReflectionV . Map.mapKeys (\(Position2 x y) -> Position2 y x)
+findSmudgeV :: Grid -> Maybe Int
+findSmudgeV g = find (single . smudgesV g) (xrange g)
 
-reflectionValue :: Grid -> Int
-reflectionValue g =
+single :: [a] -> Bool
+single [_] = True
+single _   = False
+
+reflectionValue :: (Grid -> Maybe Int) -> Grid -> Int
+reflectionValue f g =
   case (v, h) of
     (Just v', Nothing) -> v'
     (Nothing, Just h') -> h' * 100
     e ->
       terror $ "Unexpected reflections: " <> tshow e <> " in \n" <> displayG g
   where
-    v = findReflectionV g
-    h = findReflectionH g
+    v = f g
+    h = f $ transposeG g
 
 part1 :: [Grid] -> Int
-part1 = sum . map reflectionValue
+part1 = sum . map (reflectionValue findReflectionV)
 
-tasks = Tasks 2023 13 (CodeBlock 0) parser [Task part1 405]
+part2 :: [Grid] -> Int
+part2 = sum . map (reflectionValue findSmudgeV)
+
+tasks = Tasks 2023 13 (CodeBlock 0) parser [Task part1 405, Task part2 400]
