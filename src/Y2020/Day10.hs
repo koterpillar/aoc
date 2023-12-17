bacf29c7333df69ffc8d848bdfc861ec58b21c09
Alexey Kotlyarov <a@koterpillar.com>
2023-12-17 21:06:12 +1100

    Optimise and remove old memoization

--- src/Y2020/Day10.hs
+++ src/Y2020/Day10.hs
@@ -1,25 +1,28 @@
 module Y2020.Day10 where
 
+import qualified Data.Map.Strict as Map
+
 import           AOC
+import           Memo
 import           Utils
 
 oneAndThree :: [Int] -> Int
 oneAndThree adapters = countIf (== 1) stepups * countIf (== 3) stepups
   where
-    stepups = zipWith (-) (tail joltages) joltages
+    stepups = zipWithTail subtract joltages
     joltages = 0 : sort adapters ++ [maximum adapters + 3]
 
 countChains :: [Int] -> Int
-countChains allAdapters = memoFix2 go 0 $ sort allAdapters
+countChains allAdapters = go (Map.singleton 0 1) $ sort allAdapters
   where
     target = maximum allAdapters + 3
-    go :: (Int -> [Int] -> Int) -> Int -> [Int] -> Int
-    go _ v []
-      | target - v > 3 = 0
-      | otherwise = 1
-    go igo v (x:xs)
-      | x - v > 3 = 0
-      | otherwise = igo v xs + igo x xs
+    go :: Map Int Int -> [Int] -> Int
+    go v [] = sum $ Map.filterWithKey (closeTo target) v
+    go v (x:xs) = go (v1 `mapSum` Map.singleton x r) xs
+      where
+        v1 = Map.filterWithKey (closeTo x) v
+        r = sum v1
+    closeTo x k _ = x - k <= 3
 
 tasks =
   Tasks
