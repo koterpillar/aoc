27490d90321aba2cd9e50a61970d8da90625febb
Alexey Kotlyarov <a@koterpillar.com>
2023-12-18 21:04:14 +1100

    Ranges

---
+++ src/Ranges.hs
@@ -0,0 +1,62 @@
+module Ranges where
+
+import           Data.List       (uncons)
+import           Data.List.Extra (unsnoc)
+
+newtype Ranges = Ranges
+  { toList :: [(Int, Int)]
+  } deriving (Ord, Eq, Show)
+
+empty :: Ranges
+empty = Ranges []
+
+_mk1 :: Int -> Int -> (Int, Int)
+_mk1 a b
+  | a == b = error $ "singleton: empty interval " <> show (a, b)
+  | a > b = error $ "singleton: negative interval " <> show (a, b)
+  | otherwise = (a, b)
+
+fromList :: [(Int, Int)] -> Ranges
+fromList = Ranges . map (uncurry _mk1)
+
+singleton :: Int -> Int -> Ranges
+singleton a b = Ranges [_mk1 a b]
+
+_apply :: (Int -> Int -> Ranges -> Ranges) -> Ranges -> Ranges -> Ranges
+_apply f y x = foldr (uncurry f) x $ toList y
+
+_add1 :: Int -> Int -> Ranges -> Ranges
+_add1 a b = fromList . go a b . toList
+  where
+    go a b [] = [(a, b)]
+    go a b ((c, d):rs)
+      | b < c = (a, b) : (c, d) : rs
+      | d < a = (c, d) : go a b rs
+      | otherwise = go (min a c) (max b d) rs
+
+union :: Ranges -> Ranges -> Ranges
+union = _apply _add1
+
+_remove1 :: Int -> Int -> Ranges -> Ranges
+_remove1 a b = fromList . go . toList
+  where
+    go [] = []
+    go ((c, d):rs)
+      | b <= c = (c, d) : rs
+      | d <= a = (c, d) : go rs
+      | a <= c && d <= b = go rs
+      | a <= c = (b, d) : go rs
+      | d <= b = (c, a) : go rs
+      | otherwise = (c, a) : (b, d) : go rs
+
+subtract :: Ranges -> Ranges -> Ranges
+subtract = _apply _remove1
+
+minimum :: Ranges -> Maybe Int
+minimum = fmap (fst . fst) . uncons . toList
+
+maximum :: Ranges -> Maybe Int
+maximum = fmap (snd . snd) . unsnoc . toList
+
+length :: Ranges -> Int
+length = sum . map (uncurry Prelude.subtract) . toList
--- src/Y2023/Day05.hs
+++ src/Y2023/Day05.hs
@@ -1,82 +1,32 @@
 module Y2023.Day05 where
 
-import qualified Data.Map     as Map
-import qualified Data.Set     as Set
-import qualified Data.Text    as Text
+import qualified Data.Map  as Map
+import qualified Data.Set  as Set
+import qualified Data.Text as Text
 
 import           AOC
-import           GHC.TypeLits (Div)
+import qualified Ranges
+import           Ranges    (Ranges)
 import           Utils
 
-newtype Division =
-  Division
-    { dRanges :: [(Int, Int)]
-    }
-  deriving (Ord, Eq, Show)
+str1 :: [Int] -> Ranges
+str1 = foldr (\x -> Ranges.union (Ranges.singleton x $ succ x)) Ranges.empty
 
-mkDivision :: Text -> [(Int, Int)] -> Division
-mkDivision w = Division . map (mkRange w)
+str2 :: [Int] -> Ranges
+str2 []      = Ranges.empty
+str2 (x:l:r) = Ranges.union (Ranges.singleton x $ x + l) (str2 r)
 
-mkRange :: Text -> (Int, Int) -> (Int, Int)
-mkRange w (a, b)
-  | a == b = terror $ "mkRange: " <> w <> ": empty interval " <> tshow (a, b)
-  | a > b = terror $ "mkRange: " <> w <> ": negative interval " <> tshow (a, b)
-  | otherwise = (a, b)
+data Offset = Offset
+  { oDst :: Int
+  , oSrc :: Int
+  , oLen :: Int
+  } deriving (Ord, Eq, Show)
 
-dEmpty :: Division
-dEmpty = Division []
+oSrcRange :: Offset -> Ranges
+oSrcRange o = Ranges.singleton (oSrc o) (oSrc o + oLen o)
 
-dSingleton :: (Int, Int) -> Division
-dSingleton = Division . (: [])
-
-dAdd1 :: (Int, Int) -> Division -> Division
-dAdd1 r (Division rs) = mkDivision "dAdd1" $ go r rs
-  where
-    go r [] = [r]
-    go (a, b) ((c, d):rs)
-      | b < c = (a, b) : (c, d) : rs
-      | d < a = (c, d) : go (a, b) rs
-      | otherwise = go (min a c, max b d) rs
-
-dAdd :: Division -> Division -> Division
-dAdd d = foldr dAdd1 d . dRanges
-
-dRemove :: (Int, Int) -> Division -> Division
-dRemove (a, b) (Division rs) = mkDivision "dRemove" $ go rs
-  where
-    go [] = []
-    go ((c, d):rs)
-      | b <= c = (c, d) : rs
-      | d <= a = (c, d) : go rs
-      | a <= c && d <= b = go rs
-      | a <= c = mkRange "dRemove 1" (b, d) : go rs
-      | d <= b = mkRange "dRemove 2" (c, a) : go rs
-      | otherwise =
-        mkRange "dRemove 3a" (c, a) : mkRange "dRemove 3b" (b, d) : go rs
-
-dMinimum :: Division -> Maybe Int
-dMinimum = fmap fst . listToMaybe . dRanges
-
-str1 :: [Int] -> Division
-str1 = foldr (\x -> dAdd1 (x, x + 1)) dEmpty
-
-str2 :: [Int] -> Division
-str2 []      = dEmpty
-str2 (x:l:r) = dAdd1 (x, x + l) (str2 r)
-
-data Offset =
-  Offset
-    { oDst :: Int
-    , oSrc :: Int
-    , oLen :: Int
-    }
-  deriving (Ord, Eq, Show)
-
-oRemove :: Offset -> Division -> Division
-oRemove o = dRemove (oSrc o, oSrc o + oLen o)
-
-oIntersect :: Offset -> Division -> Division
-oIntersect o (Division rs) = mkDivision "oIntersect" $ go rs
+oIntersect :: Offset -> Ranges -> Ranges
+oIntersect o = Ranges.fromList . go . Ranges.toList
   where
     go [] = []
     go ((a, b):rs)
@@ -84,46 +34,52 @@ oIntersect o (Division rs) = mkDivision "oIntersect" $ go rs
       | oSrc o + oLen o < a = []
       | otherwise = (max a (oSrc o), min b (oSrc o + oLen o)) : go rs
 
-oShift :: Offset -> Division -> Division
-oShift o (Division rs) = mkDivision "oShift" $ map shift rs
+oShift :: Offset -> Ranges -> Ranges
+oShift o = Ranges.fromList . map shift . Ranges.toList
   where
     shift (a, b) = (a + c, b + c)
     c = oDst o - oSrc o
 
-newtype Transform =
-  Transform
-    { tOffsets :: [Offset]
-    }
-  deriving (Ord, Eq, Show)
+newtype Transform = Transform
+  { tOffsets :: [Offset]
+  } deriving (Ord, Eq, Show)
 
-tApply :: Transform -> Division -> Division
-tApply (Transform offsets) d = go offsets d dEmpty
+tApply :: Transform -> Ranges -> Ranges
+tApply (Transform offsets) d = go offsets d Ranges.empty
   where
-    go [] d rd     = dAdd d rd
-    go (o:os) d rd = go os (oRemove o d) (dAdd (oShift o $ oIntersect o d) rd)
+    go [] d rd = Ranges.union d rd
+    go (o:os) d rd =
+      go os (Ranges.subtract or d) (Ranges.union (oShift o $ oIntersect o d) rd)
+      where
+        or = oSrcRange o
 
-tApplyAll :: [Transform] -> Division -> Division
+tApplyAll :: [Transform] -> Ranges -> Ranges
 tApplyAll transforms s = foldl' (flip tApply) s transforms
 
-data Problem =
-  Problem
-    { pSeeds      :: [Int]
-    , pTransforms :: [Transform]
-    }
-  deriving (Ord, Eq, Show)
+data Problem = Problem
+  { pSeeds      :: [Int]
+  , pTransforms :: [Transform]
+  } deriving (Ord, Eq, Show)
 
 parser =
-  lineGroupsP &* unconsP &*
-  (uncurry Problem <$>
-   ((singleP &* pureP (Text.drop 7) &* integersP " ") &=
-    traverseP
-      (pureP tail &*
-       (Transform <$>
-        traverseP (tsplitP " " &* ap3P Offset integerP integerP integerP)))))
+  lineGroupsP
+    &* unconsP
+    &* (uncurry Problem
+          <$> ((singleP &* pureP (Text.drop 7) &* integersP " ")
+                 &= traverseP
+                      (pureP tail
+                         &* (Transform
+                               <$> traverseP
+                                     (tsplitP " "
+                                        &* ap3P
+                                             Offset
+                                             integerP
+                                             integerP
+                                             integerP)))))
 
-plantAll :: ([Int] -> Division) -> Problem -> Int
+plantAll :: ([Int] -> Ranges) -> Problem -> Int
 plantAll str (Problem seeds transforms) =
-  fromJustE "no minimum" $ dMinimum $ tApplyAll transforms $ str seeds
+  fromJustE "no minimum" $ Ranges.minimum $ tApplyAll transforms $ str seeds
 
 part1 = plantAll str1
 
@@ -135,7 +91,7 @@ tasks =
     5
     (CodeBlock 0)
     parser
-    [ Assert "dMinimum" (Just 20) (dMinimum $ dAdd1 (20, 30) dEmpty)
+    [ Assert "dMinimum" (Just 20) (Ranges.minimum $ Ranges.singleton 20 30)
     , AssertExample
         "tApply 1 1"
         (str1 [81])
