572d9b970776a825cdba54cf88e3f71a705ef501
Alexey Kotlyarov <a@koterpillar.com>
2023-12-21 17:02:57 +1100

    not clever enough

--- src/Y2023/Day21.hs
+++ src/Y2023/Day21.hs
@@ -34,18 +34,51 @@ findGnome i = (g, start)
     (start, Gnome) = fromSingleE "gnome" $ Map.toList $ Map.filter (== Gnome) i
     g = void $ Map.filter (/= Gnome) i
 
-step :: Grid2 () -> Set Position2 -> Set Position2
-step g =
-  Set.fromList
-    . filter (`Map.notMember` g)
-    . concatMap (\p -> [walk d p | d <- allDir4])
-    . Set.toList
+type Pt = (Position2, Position2) -- (position inside grid, position in the infinite grid of grids)
+
+type St = Map Position2 (Set Position2) -- key = inside grid, values = positions in the infinite grid of grids
+
+type Wlk = (Direction4 -> Position2 -> Maybe Pt)
+
+step :: Wlk -> Grid2 () -> St -> St
+step wlk g st =
+  Map.fromListWith Set.union $ do
+    (p, gps) <- Map.toList st
+    d <- allDir4
+    (p', dgp) <- toList $ wlk d p
+    guard $ Map.notMember p' g
+    let gps' = Set.map (pointPlus dgp) gps
+    pure (p', gps')
+
+reachableInSteps :: ((Position2, Position2) -> Wlk) -> Int -> Input -> Int
+reachableInSteps genWlk n i = sum $ fmap Set.size result
+  where
+    ib = inputBounds i
+    (g, gnome) = findGnome i
+    start = Map.singleton gnome (Set.singleton $ Position2 0 0)
+    wlk = genWlk ib
+    result = iterateNL n (step wlk g) start
+
+wlk1 :: (Position2, Position2) -> Wlk
+wlk1 ib d p
+  | insideBounds ib p' = Just (p', Position2 0 0)
+  | otherwise = Nothing
+  where
+    p' = walk d p
 
 part1 :: Int -> Input -> Int
-part1 n i = length result
+part1 = reachableInSteps wlk1
+
+wlk2 :: (Position2, Position2) -> Wlk
+wlk2 ib d p = Just (Position2 x'' y'', Position2 dx dy)
   where
-    (g, start) = second Set.singleton $ findGnome i
-    result = iterateNL n (step g) start
+    (Position2 0 0, Position2 xmax ymax) = ib
+    Position2 x' y' = walk d p
+    (dx, x'') = divMod x' (succ xmax)
+    (dy, y'') = divMod y' (succ ymax)
+
+part2 :: Int -> Input -> Int
+part2 = reachableInSteps wlk2
 
 tasks =
   Tasks
@@ -53,4 +86,13 @@ tasks =
     21
     (CodeBlock 0)
     parser
-    [AssertExample "part 1" 16 (part1 6), taskBlind (part1 64) & taskPart 1]
+    [ AssertExample "part 1" 16 $ part1 6
+    , taskBlind (part1 64) & taskPart 1
+    , AssertExample "part 2 1" 2 $ part2 1
+    , AssertExample "part 2 6" 16 $ part2 6
+    , AssertExample "part 2 10" 50 $ part2 10
+    , AssertExample "part 2 100" 6536 $ part2 100
+    , AssertExample "part 2 500" 167004 $ part2 500
+    -- , AssertExample "part 2 1000" 668697 $ part2 1000
+    -- , taskBlind (part2 26501365) & taskPart 2
+    ]
