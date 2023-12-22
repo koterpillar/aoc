afe43c6eeb65b252ade17bf599d57f225ad4a73d
Alexey Kotlyarov <a@koterpillar.com>
2023-12-22 15:25:05 +1100

    Remove broken part 2

--- src/Y2023/Day21.hs
+++ src/Y2023/Day21.hs
@@ -2,9 +2,9 @@
 
 module Y2023.Day21 where
 
-import qualified Data.Map               as Map
-import qualified Data.Set               as Set
-import qualified Data.Text              as Text
+import qualified Data.Map  as Map
+import qualified Data.Set  as Set
+import qualified Data.Text as Text
 
 import           AOC
 import           Grid
@@ -13,11 +13,13 @@ import           Utils
 data GardenItem
   = Sand
   | Gnome
+  | Reach
   deriving (Ord, Eq, Show, Enum, Bounded)
 
 instance GridItem GardenItem where
   showInGrid Sand  = '#'
   showInGrid Gnome = 'S'
+  showInGrid Reach = 'O'
 
 type Input = Grid2 GardenItem
 
@@ -31,58 +33,46 @@ inputMaxBound input = Position2 (succ xmax) (succ ymax)
 
 type Grid = Grid2 ()
 
-findGnome :: Input -> (Grid, Position2)
-findGnome i = (g, start)
+findGnomeCenter :: Input -> Grid
+findGnomeCenter i = g
   where
     (start, Gnome) = fromSingleE "gnome" $ Map.toList $ Map.filter (== Gnome) i
-    g = void $ Map.filter (/= Gnome) i
+    g = Map.mapKeys (`pointMinus` start) $ void $ Map.filter (/= Gnome) i
 
-type It = (Position2, Set Position2) -- (position inside grid, positions in the infinite grid of grids)
+stepPoint :: Grid -> Position2 -> [Position2]
+stepPoint g p = do
+  d <- allDir4
+  let p' = walk d p
+  guard $ Map.notMember p' g
+  pure p'
 
-type St = Map Position2 (Set Position2) -- key = inside grid, values = positions in the infinite grid of grids
+step1 :: Grid -> Set Position2 -> Set Position2
+step1 g st =
+  Set.fromList $ do
+    p <- Set.toList st
+    stepPoint g p
 
-type Wlk = (Direction4 -> Position2 -> Maybe (Position2, Position2))
+stepN :: Grid -> Int -> Set Position2 -> Set Position2
+stepN _ 0 ps = ps
+stepN g n ps = stepN g (pred n) $ step1 g ps
 
-step :: Wlk -> St -> St
-step wlk st =
-  Map.fromListWith Set.union $ do
-    (p, gps) <- Map.toList st
-    d <- allDir4
-    (p', dgp) <- toList $ wlk d p
-    let gps' = Set.map (pointPlus dgp) gps
-    pure (p', gps')
+nubMerge :: Ord a => [a] -> [a] -> [a]
+nubMerge a b = nubOrd $ a ++ b
 
-reachableInSteps :: (Grid -> Position2 -> Wlk) -> Int -> Input -> Int
-reachableInSteps genWlk n i = sum $ fmap Set.size result
+mkGarden :: Grid -> Set Position2 -> Input
+mkGarden g st = Map.map (const Sand) g `Map.union` Map.fromSet (const Reach) st
+
+reachableInSteps :: Int -> Input -> Set Position2
+reachableInSteps n i =
+  ttraceF (displayG . mkGarden g) $ stepN g n (Set.singleton $ Position2 0 0)
   where
     im = inputMaxBound i
-    (g, gnome) = findGnome i
-    start = Map.singleton gnome (Set.singleton $ Position2 0 0)
-    wlk = genWlk g im
-    result = iterateNL n (step wlk) start
-
-wlk1 :: Grid -> Position2 -> Wlk
-wlk1 g im d p =
-  wlk2 g im d p >>= \case
-    r@(p', Position2 0 0) -> Just r
-    _ -> Nothing
-
-wlk2 :: Grid -> Position2 -> Wlk
-wlk2 g im d p = do
-  let Position2 xmax ymax = im
-  let Position2 x' y' = walk d p
-  let (dx, x'') = divMod x' (succ xmax)
-  let (dy, y'') = divMod y' (succ ymax)
-  let p'' = Position2 x'' y''
-  guard $ Map.notMember p'' g
-  let dp = Position2 dx dy
-  pure (p'', dp)
+    g = findGnomeCenter i
 
 part1 :: Int -> Input -> Int
-part1 = reachableInSteps wlk1
+part1 n = length . reachableInSteps n
 
-part2 :: Int -> Input -> Int
-part2 = reachableInSteps wlk2
+part2 = error "error"
 
 tasks =
   Tasks
@@ -93,6 +83,7 @@ tasks =
     [ AssertExample "part 1" 16 $ part1 6
     , taskBlind (part1 64) & taskPart 1
     , AssertExample "part 2 1" 2 $ part2 1
+    , AssertExample "part 2 2" 4 $ part2 2
     , AssertExample "part 2 6" 16 $ part2 6
     , AssertExample "part 2 10" 50 $ part2 10
     , AssertExample "part 2 100" 6536 $ part2 100
