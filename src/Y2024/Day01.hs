3d3eba5b365fcf98c0fb31bbe45f097ea943a6af
Alexey Kotlyarov <a@koterpillar.com>
2024-12-01 16:14:18 +1100

    2024 day 1

---
+++ src/Y2024/Day01.hs
@@ -0,0 +1,33 @@
+module Y2024.Day01 where
+
+import qualified Data.Map  as Map
+import qualified Data.Set  as Set
+import qualified Data.Text as Text
+
+import           AOC
+import           Utils
+
+parser :: Parser Text ([Int], [Int])
+parser = fmap unzip $ linesP &** (wordsP &* (integerP &+ integerP))
+
+
+part1 (as, bs) = sum $ map abs $ zipWith (-) as' bs'
+  where
+    as' = sort as
+    bs' = sort bs
+
+part2 :: ([Int], [Int]) -> Int
+part2 (as, bs) = sum $ map similarity as
+  where
+    bs' = mapFromListCount bs
+    similarity a = a * fromMaybe 0 (Map.lookup a bs')
+
+tasks =
+  Tasks
+    2024
+    1
+    (CodeBlock 0)
+    parser
+    [ task part1 11 & taskPart 1
+    , task part2 31 & taskPart 2
+    ]
