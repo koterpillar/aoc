52b520cc9be75fdb7bac3a9ab509629035c1b845
Alexey Kotlyarov <a@koterpillar.com>
2024-12-02 16:40:27 +1100

    2024 day 2

---
+++ src/Y2024/Day02.hs
@@ -0,0 +1,32 @@
+module Y2024.Day02 where
+
+import qualified Data.Map  as Map
+import qualified Data.Set  as Set
+import qualified Data.Text as Text
+
+import           AOC
+import           Utils
+
+parser :: Parser Text [[Int]]
+parser = linesP &** integersSpaceP
+
+part1 = countIf $ isSafeRev False
+
+part2 = countIf $ isSafeRev True
+
+isSafeRev damp x = isSafe1 damp x || isSafe1 damp (reverse x)
+
+isSafe1 False = isSafe
+isSafe1 True  = any (isSafe . snd) . picks
+
+isSafe = all (uncurry safeStep) . zipTail
+
+safeStep x1 x2 = x1 > x2 && x1 <= x2 + 3
+
+tasks =
+  Tasks
+    2024
+    2
+    (CodeBlock 0)
+    parser
+    [task part1 2 & taskPart 1, task part2 4 & taskPart 2]
