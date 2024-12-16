f2272be9bc50c2bf6a94d7686732b6919fbcbcbd
Alexey Kotlyarov <a@koterpillar.com>
2024-12-16 17:07:04 +1100

    part 2

--- src/Y2024/Day16.hs
+++ src/Y2024/Day16.hs
@@ -34,15 +34,39 @@ data Deer =
 instance Hashable Deer where
   hashWithSalt s (Deer p d) = hashWithSalt s (p, d)
 
-bestPath :: Grid -> Maybe [Deer]
-bestPath g = (start :) <$> aStarGoal (moves g) score1 remaining start
+type Path = [Deer]
+
+type Seen = Set Position2
+
+mkSeen :: Path -> Seen
+mkSeen = Set.fromList . map (\(Deer p _) -> p)
+
+bestPath :: Seen -> Grid -> Maybe Path
+bestPath ps g = (start :) <$> aStarGoal (moves g) (score2 ps) remaining start
   where
     start = Deer deerStart E
     deerStart = fromJustE "deerStart" $ findTuple (== Start) $ Map.toList g
     deerEnd = fromJustE "deerEnd" $ findTuple (== End) $ Map.toList g
     remaining (Deer p _) = manhattanDistance p deerEnd
 
-moves :: Grid -> Deer -> [Deer]
+bestPaths :: Grid -> [Path]
+bestPaths g =
+  case bestPath mempty g of
+    Nothing -> []
+    Just p1 -> p1 : unfoldr go (mkSeen p1)
+      where go ps =
+              case bestPath ps g of
+                Nothing -> Nothing
+                Just p
+                  | score p == p1score ->
+                    let s = mkSeen p
+                     in if s `Set.isSubsetOf` ps
+                          then Nothing
+                          else Just (p, s <> ps)
+                  | otherwise -> Nothing
+            p1score = score p1
+
+moves :: Grid -> Deer -> Path
 moves g (Deer p d) =
   turns ++ [Deer (walk d p) d | Map.lookup (walk d p) g /= Just Wall]
   where
@@ -53,11 +77,29 @@ score1 (Deer p1 d1) (Deer p2 d2)
   | d1 == d2 = 1
   | otherwise = 1000
 
-score :: [Deer] -> Int
+score2 :: Seen -> Deer -> Deer -> Int
+score2 seen d1 d2 = score1 d1 d2 * 1000000 + seenScore d1 + seenScore d2
+  where
+    seenScore (Deer p _) =
+      if Set.member p seen
+        then 1
+        else 0
+
+score :: Path -> Int
 score = sum . zipWithTail score1
 
 part1 :: Grid -> Int
-part1 = score . fromJustE "no best path" . bestPath
+part1 = score . fromJustE "no best path" . bestPath mempty
+
+part2 :: Grid -> Int
+part2 g =
+  (length . Set.unions . map mkSeen . ttraceF (displayPath g . join) . bestPaths)
+    g
+
+displayPath :: Grid -> Path -> Text
+displayPath g s = displayG $ fmap showInGrid g `Map.union` Map.fromList g'
+  where
+    g' = [(p, showInGrid d) | Deer p d <- s]
 
 tasks =
   Tasks
@@ -65,4 +107,8 @@ tasks =
     16
     (CodeBlock 0)
     parser
-    [task part1 7036 & taskPart 1, task part1 11048 & taskScraper (CodeBlock 2)]
+    [ task part1 7036 & taskPart 1
+    , task part1 11048 & taskScraper (CodeBlock 2)
+    , task part2 45 & taskPart 2
+    , task part2 64 & taskScraper (CodeBlock 2)
+    ]
