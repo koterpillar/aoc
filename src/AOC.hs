70614c4f0df940344a844b98c8f168109622fbc6
Alexey Kotlyarov <a@koterpillar.com>
2025-12-06 19:17:47 +1100

    Formatting

--- src/AOC.hs
+++ src/AOC.hs
@@ -1,46 +1,51 @@
 {-# LANGUAGE GADTs #-}
 
-module AOC
-  ( getExample
-  , getInput
-  , ExampleScraper(..)
-  , Tasks(..)
-  , Task(..)
-  , task
-  , taskBlind
-  , taskPart
-  , taskScraper
-  , taskTimeout
-  , processTasks
-  , module Miniparse
-  ) where
+module AOC (
+    getExample,
+    getInput,
+    ExampleScraper (..),
+    Tasks (..),
+    Task (..),
+    task,
+    taskBlind,
+    taskPart,
+    taskScraper,
+    taskTimeout,
+    processTasks,
+    module Miniparse,
+) where
 
-import qualified Data.Text                   as Text
-import qualified Data.Text.Encoding          as Text
-import qualified Data.Text.IO                as Text
-import qualified Data.Text.Lazy              as LazyText
-import qualified Data.Text.Lazy.Builder      as LazyText
+import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
+import qualified Data.Text.IO as Text
+import qualified Data.Text.Lazy as LazyText
+import qualified Data.Text.Lazy.Builder as LazyText
 
-import           Data.String                 (IsString (..))
+import Data.String (IsString (..))
 
-import           Data.Time                   (UTCTime (..), getCurrentTime,
-                                              toGregorian)
+import Data.Time (
+    UTCTime (..),
+    getCurrentTime,
+    toGregorian,
+ )
 
-import           GHC.IO.Exception            (IOException)
+import GHC.IO.Exception (IOException)
 
-import           HTMLEntities.Decoder        (htmlEncodedText)
+import HTMLEntities.Decoder (htmlEncodedText)
 
-import           Network.HTTP.Client.Conduit (Request (..))
-import           Network.HTTP.Simple
+import Network.HTTP.Client.Conduit (Request (..))
+import Network.HTTP.Simple
 
-import           System.Directory            (createDirectoryIfMissing,
-                                              doesFileExist)
-import           System.FilePath             (takeDirectory)
-import           System.IO                   (isEOF)
-import           System.Timeout
+import System.Directory (
+    createDirectoryIfMissing,
+    doesFileExist,
+ )
+import System.FilePath (takeDirectory)
+import System.IO (isEOF)
+import System.Timeout
 
-import           Miniparse
-import           Utils
+import Miniparse
+import Utils
 
 -- | https://www.reddit.com/r/adventofcode/comments/z9dhtd/please_include_your_contact_info_in_the_useragent/
 userAgent :: Text
@@ -48,83 +53,83 @@ userAgent = "github.com/koterpillar/aoc by a@koterpillar.com"
 
 baseURL :: Integer -> Int -> Text
 baseURL year day =
-  "https://adventofcode.com/" <> tshow year <> "/day/" <> tshow day
+    "https://adventofcode.com/" <> tshow year <> "/day/" <> tshow day
 
 readIfExists :: Text -> IO (Maybe Text)
 readIfExists fileName = do
-  let fileName' = Text.unpack fileName
-  exists <- doesFileExist fileName'
-  if exists
-    then Just <$> Text.readFile fileName'
-    else pure Nothing
+    let fileName' = Text.unpack fileName
+    exists <- doesFileExist fileName'
+    if exists
+        then Just <$> Text.readFile fileName'
+        else pure Nothing
 
 readSession :: IO (Maybe Text)
 readSession = fmap ttrim <$> readIfExists ".session-cookie"
 
 addHeader :: String -> Text -> Request -> Request
 addHeader name value request =
-  request
-    { requestHeaders =
-        (fromString name, Text.encodeUtf8 value) : requestHeaders request
-    }
+    request
+        { requestHeaders =
+            (fromString name, Text.encodeUtf8 value) : requestHeaders request
+        }
 
 aocRequest :: Text -> IO Text
 aocRequest url = parseRequestThrow (Text.unpack url) >>= aocRequest'
 
 aocRequest' :: Request -> IO Text
 aocRequest' request = do
-  session <- readSession
-  let request' =
-        addHeader "User-Agent" userAgent
-          $ foldr (addHeader "Cookie" . ("session=" <>)) request session
-  response <- getResponseBody <$> httpBS request'
-  pure $ Text.decodeUtf8 response
+    session <- readSession
+    let request' =
+            addHeader "User-Agent" userAgent $
+                foldr (addHeader "Cookie" . ("session=" <>)) request session
+    response <- getResponseBody <$> httpBS request'
+    pure $ Text.decodeUtf8 response
 
 class CacheFileName a where
-  cacheFileName :: a -> Text
+    cacheFileName :: a -> Text
 
 withCacheFile' ::
-     CacheFileName a
-  => Integer
-  -> Int
-  -> a
-  -> (Maybe Text -> IO Text)
-  -> IO Text
+    (CacheFileName a) =>
+    Integer ->
+    Int ->
+    a ->
+    (Maybe Text -> IO Text) ->
+    IO Text
 withCacheFile' year day cacheKey action = do
-  let fileName =
-        ".cache/"
-          <> tshow year
-          <> "/"
-          <> tshow day
-          <> "/"
-          <> cacheFileName cacheKey
-  cached <- readIfExists fileName
-  result <- action cached
-  when (Just result /= cached) $ do
-    let fileName' = Text.unpack fileName
-    createDirectoryIfMissing True $ takeDirectory fileName'
-    Text.writeFile fileName' result
-  pure result
+    let fileName =
+            ".cache/"
+                <> tshow year
+                <> "/"
+                <> tshow day
+                <> "/"
+                <> cacheFileName cacheKey
+    cached <- readIfExists fileName
+    result <- action cached
+    when (Just result /= cached) $ do
+        let fileName' = Text.unpack fileName
+        createDirectoryIfMissing True $ takeDirectory fileName'
+        Text.writeFile fileName' result
+    pure result
 
-withCacheFile :: CacheFileName a => Integer -> Int -> a -> IO Text -> IO Text
+withCacheFile :: (CacheFileName a) => Integer -> Int -> a -> IO Text -> IO Text
 withCacheFile year day cacheKey action =
-  withCacheFile' year day cacheKey $ maybe action pure
+    withCacheFile' year day cacheKey $ maybe action pure
 
 dropAfterAll :: Text -> Text -> [Text]
 dropAfterAll marker =
-  map (Text.drop (Text.length marker) . snd) . Text.breakOnAll marker
+    map (Text.drop (Text.length marker) . snd) . Text.breakOnAll marker
 
 dropBefore :: Text -> Text -> Text
 dropBefore marker contents =
-  case Text.breakOn marker contents of
-    (result, rest)
-      | Text.null rest ->
-        error
-          $ "Could not find marker: "
-              <> Text.unpack marker
-              <> " in contents: "
-              <> Text.unpack contents
-      | otherwise -> result
+    case Text.breakOn marker contents of
+        (result, rest)
+            | Text.null rest ->
+                error $
+                    "Could not find marker: "
+                        <> Text.unpack marker
+                        <> " in contents: "
+                        <> Text.unpack contents
+            | otherwise -> result
 
 htmlDecode :: Text -> Text
 htmlDecode = LazyText.toStrict . LazyText.toLazyText . htmlEncodedText
@@ -140,15 +145,15 @@ codeBlockEnd = "</code></pre>"
 
 codeBlocks :: Text -> [Text]
 codeBlocks =
-  map (removeTags . htmlDecode . dropBefore codeBlockEnd)
-    . dropAfterAll codeBlockBegin
+    map (removeTags . htmlDecode . dropBefore codeBlockEnd)
+        . dropAfterAll codeBlockBegin
 
 inlineCode :: Text -> [Text]
 inlineCode =
-  filter ((<) 10 . Text.length)
-    . map (removeTags . htmlDecode . dropBefore exampleEnd)
-    . dropAfterAll inlineCodeBegin
-    . Text.replace codeBlockBegin ""
+    filter ((<) 10 . Text.length)
+        . map (removeTags . htmlDecode . dropBefore exampleEnd)
+        . dropAfterAll inlineCodeBegin
+        . Text.replace codeBlockBegin ""
   where
     inlineCodeBegin = "<code>"
     exampleEnd = "</code>"
@@ -163,125 +168,129 @@ answers :: Text -> [Text]
 answers = map (dropBefore answerEnd) . dropAfterAll answerBegin
 
 data ExampleScraper
-  = ShowExamples
-  | CodeBlock Int
-  | LastCodeBlock
-  | InlineCode Int
-  | LastInlineCode
-  | Inline Text
-  deriving (Show)
+    = ShowExamples
+    | CodeBlock Int
+    | LastCodeBlock
+    | InlineCode Int
+    | LastInlineCode
+    | Inline Text
+    deriving (Show)
 
 instance CacheFileName ExampleScraper where
-  cacheFileName = ("example-" <>) . Text.replace " " "-" . terase "\"" . tshow
+    cacheFileName = ("example-" <>) . Text.replace " " "-" . terase "\"" . tshow
 
 getExample :: Integer -> Int -> ExampleScraper -> IO Text
 getExample year day scraper =
-  withCacheFile year day scraper $ do
-    page <- aocRequest $ baseURL year day
-    pure $! selectExample scraper page
+    withCacheFile year day scraper $ do
+        page <- aocRequest $ baseURL year day
+        pure $! selectExample scraper page
 
 showExamples :: Text -> Text
 showExamples page =
-  Text.unlines $ "Examples found:" : codeBlocksMsg ++ inlineCodeMsg
+    Text.unlines $ "Examples found:" : codeBlocksMsg ++ inlineCodeMsg
   where
     codeBlocksMsg = labelExamples "CodeBlock" $ codeBlocks page
     inlineCodeMsg = labelExamples "InlineCode" $ inlineCode page
     labelExamples itemName =
-      zipWith
-        (\i e -> "--- " <> itemName <> " " <> tshow i <> " ---\n" <> e)
-        [0 ..]
+        zipWith
+            (\i e -> "--- " <> itemName <> " " <> tshow i <> " ---\n" <> e)
+            [0 ..]
 
 selectExample :: ExampleScraper -> Text -> Text
-selectExample ShowExamples   = terror . showExamples
-selectExample (CodeBlock n)  = indexE "code blocks" n . codeBlocks
-selectExample LastCodeBlock  = last . codeBlocks
+selectExample ShowExamples = terror . showExamples
+selectExample (CodeBlock n) = indexE "code blocks" n . codeBlocks
+selectExample LastCodeBlock = last . codeBlocks
 selectExample (InlineCode n) = indexE "inline code" n . inlineCode
 selectExample LastInlineCode = last . inlineCode
-selectExample (Inline text)  = const text
+selectExample (Inline text) = const text
 
 newtype Answer = Answer
-  { answerPart :: Int
-  } deriving (Eq, Ord, Show)
+    { answerPart :: Int
+    }
+    deriving (Eq, Ord, Show)
 
 instance CacheFileName Answer where
-  cacheFileName (Answer part) = "answer-" <> tshow part
+    cacheFileName (Answer part) = "answer-" <> tshow part
 
 getAnswer :: Integer -> Int -> Answer -> IO (Maybe Text)
 getAnswer year day answer =
-  discardEmpty <$> withCacheFile' year day answer fetchAnswer
+    discardEmpty <$> withCacheFile' year day answer fetchAnswer
   where
     fetchAnswer = maybe fetchAnswer' pure . ((=<<) discardEmpty)
     fetchAnswer' = do
-      page <- aocRequest $ baseURL year day
-      let as = answers page
-      let a = fromMaybe mempty $ as !? pred (answerPart answer)
-      pure $! a
+        page <- aocRequest $ baseURL year day
+        let as = answers page
+        let a = fromMaybe mempty $ as !? pred (answerPart answer)
+        pure $! a
     discardEmpty t
-      | Text.null t = Nothing
-      | otherwise = Just t
+        | Text.null t = Nothing
+        | otherwise = Just t
 
 currentYear :: IO Integer
 currentYear = do
-  (y, _, _) <- toGregorian . utctDay <$> getCurrentTime
-  pure y
+    (y, _, _) <- toGregorian . utctDay <$> getCurrentTime
+    pure y
 
-data Input =
-  Input
-  deriving (Eq, Ord, Show)
+data Input
+    = Input
+    deriving (Eq, Ord, Show)
 
 instance CacheFileName Input where
-  cacheFileName Input = "input"
+    cacheFileName Input = "input"
 
 getInput :: Integer -> Int -> IO Text
 getInput year day =
-  withCacheFile year day Input $ aocRequest $ baseURL year day <> "/input"
+    withCacheFile year day Input $ aocRequest $ baseURL year day <> "/input"
 
 data Tasks where
-  Tasks
-    :: Integer -> Int -> ExampleScraper -> Parser Text a -> [Task a] -> Tasks
+    Tasks ::
+        Integer -> Int -> ExampleScraper -> Parser Text a -> [Task a] -> Tasks
 
-class (Eq a) =>
-      AnswerLike a
-  where
-  answerToText :: a -> Text
+class
+    (Eq a) =>
+    AnswerLike a
+    where
+    answerToText :: a -> Text
 
 instance AnswerLike Text where
-  answerToText = id
+    answerToText = id
 
 instance AnswerLike String where
-  answerToText = Text.pack
+    answerToText = Text.pack
 
 instance AnswerLike Int where
-  answerToText = Text.pack . show
+    answerToText = Text.pack . show
 
 instance AnswerLike Integer where
-  answerToText = Text.pack . show
+    answerToText = Text.pack . show
 
 instance AnswerLike () where
-  answerToText () = mempty
+    answerToText () = mempty
 
 data Task a where
-  Task :: AnswerLike b => (a -> b) -> b -> Task a
-  Task'
-    :: AnswerLike b=> { _taskFn :: a -> b
-                      , _taskExampleAnswer :: Maybe b
-                      , _taskScraper :: Maybe ExampleScraper
-                      , _taskPart :: Maybe Int
-                      , _taskTimeout :: Maybe Int}
-    -> Task a
-  Assert :: (Eq b, Show b) => Text -> b -> b -> Task a
-  AssertExample :: (Eq b, Show b) => Text -> b -> (a -> b) -> Task a
+    Task :: (AnswerLike b) => (a -> b) -> b -> Task a
+    Task' ::
+        (AnswerLike b) =>
+        { _taskFn :: a -> b
+        , _taskExampleAnswer :: Maybe b
+        , _taskScraper :: Maybe ExampleScraper
+        , _taskPart :: Maybe Int
+        , _taskTimeout :: Maybe Int
+        } ->
+        Task a
+    Assert :: (Eq b, Show b) => Text -> b -> b -> Task a
+    AssertExample :: (Eq b, Show b) => Text -> b -> (a -> b) -> Task a
 
-task :: AnswerLike b => (a -> b) -> b -> Task a
-task _taskFn a = Task' {..}
+task :: (AnswerLike b) => (a -> b) -> b -> Task a
+task _taskFn a = Task'{..}
   where
     _taskExampleAnswer = Just a
     _taskScraper = Nothing
     _taskPart = Nothing
     _taskTimeout = Nothing
 
-taskBlind :: AnswerLike b => (a -> b) -> Task a
-taskBlind _taskFn = Task' {..}
+taskBlind :: (AnswerLike b) => (a -> b) -> Task a
+taskBlind _taskFn = Task'{..}
   where
     _taskExampleAnswer = Nothing
     _taskScraper = Nothing
@@ -289,79 +298,79 @@ taskBlind _taskFn = Task' {..}
     _taskTimeout = Nothing
 
 taskScraper :: ExampleScraper -> Task a -> Task a
-taskScraper scraper t@Task' {} = t {_taskScraper = Just scraper}
+taskScraper scraper t@Task'{} = t{_taskScraper = Just scraper}
 taskScraper _ t = terror $ "Cannot override scraper for: " <> taskName t
 
 taskPart :: Int -> Task a -> Task a
-taskPart part t@Task' {} = t {_taskPart = Just part}
-taskPart _ t             = terror $ "Cannot override part for: " <> taskName t
+taskPart part t@Task'{} = t{_taskPart = Just part}
+taskPart _ t = terror $ "Cannot override part for: " <> taskName t
 
 taskNoPart :: Task a -> Task a
-taskNoPart t@Task' {} = t {_taskPart = Nothing}
-taskNoPart t          = terror $ "Cannot remove part from: " <> taskName t
+taskNoPart t@Task'{} = t{_taskPart = Nothing}
+taskNoPart t = terror $ "Cannot remove part from: " <> taskName t
 
 taskTimeout :: Int -> Task a -> Task a
-taskTimeout timeout t@Task' {} = t {_taskTimeout = Just timeout}
-taskTimeout _ t                = terror $ "Cannot override timeout for: " <> taskName t
+taskTimeout timeout t@Task'{} = t{_taskTimeout = Just timeout}
+taskTimeout _ t = terror $ "Cannot override timeout for: " <> taskName t
 
 taskName :: Task a -> Text
-taskName Task {}                  = "Task"
-taskName Task' {}                 = "Task"
-taskName (Assert name _ _)        = name
+taskName Task{} = "Task"
+taskName Task'{} = "Task"
+taskName (Assert name _ _) = name
 taskName (AssertExample name _ _) = name
 
 getTimeout :: Task a -> Int -- seconds
 getTimeout (Task' _ _ _ _ (Just t)) = t
-getTimeout _                        = 40
+getTimeout _ = 40
 
 processTasks :: Tasks -> IO ()
 processTasks (Tasks year day scraper parser tasks) = do
-  Text.putStrLn $ "Year " <> tshow year <> ", day " <> tshow day
-  for_ tasks $ \task -> do
-    let timeoutValue = getTimeout task
-    result <-
-      timeout (timeoutValue * 1000000)
-        $ processTask year day scraper parser task
-    when (isNothing result)
-      $ terror
-      $ taskName task <> ": timeout after " <> tshow timeoutValue <> " seconds"
+    Text.putStrLn $ "Year " <> tshow year <> ", day " <> tshow day
+    for_ tasks $ \task -> do
+        let timeoutValue = getTimeout task
+        result <-
+            timeout (timeoutValue * 1000000) $
+                processTask year day scraper parser task
+        when (isNothing result) $
+            terror $
+                taskName task <> ": timeout after " <> tshow timeoutValue <> " seconds"
 
 processTask ::
-     Integer -> Int -> ExampleScraper -> Parser Text a -> Task a -> IO ()
+    Integer -> Int -> ExampleScraper -> Parser Text a -> Task a -> IO ()
 processTask year day globalScraper parser (Task solve expected) =
-  processTask
-    year
-    day
-    globalScraper
-    parser
-    (Task' solve (Just expected) Nothing Nothing Nothing)
+    processTask
+        year
+        day
+        globalScraper
+        parser
+        (Task' solve (Just expected) Nothing Nothing Nothing)
 processTask year day globalScraper parser (Task' solve exampleAnswer taskScraper part _) = do
-  let scraper = fromMaybe globalScraper taskScraper
-  example <- justParse parser <$> getExample year day scraper
-  let exampleResult = solve example
-  case exampleAnswer of
-    Just answer ->
-      assertEqual
-        "Example result"
-        (answerToText answer)
-        (answerToText exampleResult)
-    Nothing -> Text.putStrLn $ "Example result " <> answerToText exampleResult
-  input <- justParse parser <$> getInput year day
-  let result = solve input
-  Text.putStrLn $ answerToText result
-  for_ part $ \part' -> do
-    getAnswer year day (Answer part') >>= \case
-      Just answer -> assertEqual ("Answer part " <> tshow part') answer (answerToText result)
-      Nothing -> pure ()
+    let scraper = fromMaybe globalScraper taskScraper
+    example <- justParse parser <$> getExample year day scraper
+    let exampleResult = solve example
+    case exampleAnswer of
+        Just answer ->
+            assertEqual
+                "Example result"
+                (answerToText answer)
+                (answerToText exampleResult)
+        Nothing -> Text.putStrLn $ "Example result " <> answerToText exampleResult
+    input <- justParse parser <$> getInput year day
+    let result = solve input
+    Text.putStrLn $ answerToText result
+    for_ part $ \part' -> do
+        getAnswer year day (Answer part') >>= \case
+            Just answer -> assertEqual ("Answer part " <> tshow part') answer (answerToText result)
+            Nothing -> pure ()
 processTask _ _ _ _ (Assert message expected actual) =
-  assertEqual message expected actual
+    assertEqual message expected actual
 processTask year day scraper parser (AssertExample message expected fn) = do
-  example <- justParse parser <$> getExample year day scraper
-  assertEqual ("Example " <> message) expected $ fn example
+    example <- justParse parser <$> getExample year day scraper
+    assertEqual ("Example " <> message) expected $ fn example
 
 assertEqual :: (Eq a, Show a) => Text -> a -> a -> IO ()
 assertEqual message expected actual
-  | expected == actual = Text.putStrLn $ message <> " OK"
-  | otherwise =
-    terror
-      $ message <> " is " <> tshow actual <> ", but expected " <> tshow expected
+    | expected == actual = Text.putStrLn $ message <> " OK"
+    | otherwise =
+        terror $
+            message <> " is " <> tshow actual <> ", but expected " <> tshow expected
